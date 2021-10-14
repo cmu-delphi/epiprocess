f1e5bbd071196720819af9bfc88795d320531e38
Ryan Tibshirani <ryantibs@gmail.com>
2021-10-13 23:46:11 -0400

    Fix few typos

--- R/epitools.R
+++ R/epitools.R
@@ -1,9 +1,9 @@
-#' epipred: Tools for Basic Signal Processing in Epidemiology
+#' epitools: Tools for Basic Signal Processing in Epidemiology
 #'
 #' The `epitools` package provides contains tools for basic signal processing
 #' tasks in epidemiology, which includes computing percentage changes in
 #' signals, estimating derivatives, and computing correlations.
 #'
 #' @docType package
-#' @name epipred
+#' @name epitools
 NULL
