b3679a0975384e67f31ee6533b249afcf52edb81
Ryan Tibshirani <ryantibs@gmail.com>
2022-01-09 20:53:40 -0500

    epitools -> epiprocess

--- R/epiprocess.R
+++ R/epiprocess.R
@@ -1,9 +1,9 @@
-#' epitools: Tools for basic signal processing in epidemiology
+#' epiprocess: Tools for basic signal processing in epidemiology
 #'
 #' This package introduces a common data structure for epidemiological data sets
 #' measured over space and time, and offers associated utilities to perform
 #' basic signal processing tasks. 
 #'
 #' @docType package
-#' @name epitools
+#' @name epiprocess
 NULL
