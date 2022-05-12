094bf64912f842b35b955db8c2082f2a4f87da54
ChloeYou <chloeyxy@outlook.com>
2022-05-11 23:10:27 -0700

    outlier examples added

--- R/outliers.R
+++ R/outliers.R
@@ -44,6 +44,34 @@
 #'   STL decomposition.
 #' 
 #' @export
+#' @examples
+#'  library(dplyr)
+#'  
+#'  detection_methods = bind_rows(
+#'    tibble(method = "rm",
+#'           args = list(list(detect_negatives = TRUE,
+#'                            detection_multiplier = 2.5)),
+#'           abbr = "rm"),
+#'    tibble(method = "stl",
+#'           args = list(list(detect_negatives = TRUE,
+#'                            detection_multiplier = 2.5,
+#'                            seasonal_period = 7)),
+#'           abbr = "stl_seasonal"),
+#'    tibble(method = "stl",
+#'           args = list(list(detect_negatives = TRUE,
+#'                            detection_multiplier = 2.5,
+#'                            seasonal_period = NULL)),
+#'           abbr = "stl_nonseasonal"))  
+#'  
+#'  x <- jhu_csse_daily %>% 
+#'    select(geo_value,time_value,cases) %>% 
+#'    as_epi_df()%>% 
+#'    group_by(geo_value) %>%
+#'    mutate(outlier_info  = detect_outlr(
+#'      x = time_value, y = cases,
+#'      methods = detection_methods,
+#'      combiner = "median")) %>%
+#'    unnest(outlier_info)
 detect_outlr = function(x = seq_along(y), y,
                         methods = tibble::tibble(method = "rm",
                                          args = list(list()),
@@ -120,6 +148,16 @@ detect_outlr = function(x = seq_along(y), y,
 #'   `lower`, `upper`, and `replacement`.
 #'
 #' @export
+#' @examples 
+#' library(dplyr)
+# # Detect outliers based on a rolling median
+#' jhu_csse_daily %>% 
+#'   select(geo_value,time_value,cases) %>% 
+#'   as_epi_df()%>% 
+#'   group_by(geo_value) %>%
+#'   mutate(outlier_info  = detect_outlr_rm(
+#'     x = time_value, y = cases)) %>%
+#'   unnest(outlier_info)
 detect_outlr_rm = function(x = seq_along(y), y, n = 21,
                            log_transform = FALSE,
                            detect_negatives = FALSE,
@@ -208,6 +246,17 @@ detect_outlr_rm = function(x = seq_along(y), y, n = 21,
 #' @importFrom stats median
 #' @importFrom tidyselect starts_with
 #' @export
+#' @examples 
+#' library(dplyr)
+# # Detects outliers based on a seasonal-trend decomposition using LOESS 
+#' jhu_csse_daily %>% 
+#'   select(geo_value,time_value,cases) %>% 
+#'   as_epi_df()%>% 
+#'   group_by(geo_value) %>%
+#'   mutate(outlier_info  = detect_outlr_stl(
+#'     x = time_value, y = cases,
+#'     seasonal_period = 7 )) %>% # weekly seasonality for daily data
+#'   unnest(outlier_info)
 detect_outlr_stl = function(x = seq_along(y), y,
                             n_trend = 21,
                             n_seasonal = 21,
