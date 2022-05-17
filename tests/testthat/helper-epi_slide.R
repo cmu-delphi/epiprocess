462b950667c06b650eb10aa8d7ed3091a8c9449b
admin <admin@host70-185.resnet.ubc.ca>
2022-05-17 12:49:21 -0700

    Moved helper file code to test file and deleted helper file

--- tests/testthat/helper-epi_slide.R
+++
@@ -1,9 +0,0 @@
-## Create an epi. df and a function to test epi_slide with
-
-edf = dplyr::bind_rows(
-  dplyr::tibble(geo_value = "ak", time_value = as.Date("2020-01-01") + 1:200, value=1:200),
-  dplyr::tibble(geo_value = "al", time_value=as.Date("2020-01-01") + 1:5, value=-(1:5))
-) %>%
-  as_epi_df()
-
-f = function(x, ...) dplyr::tibble(value=mean(x$value), count=length(x$value))
--- tests/testthat/test-epi_slide.R
+++ tests/testthat/test-epi_slide.R
@@ -1,3 +1,13 @@
+## Create an epi. df and a function to test epi_slide with
+
+edf = dplyr::bind_rows(
+  dplyr::tibble(geo_value = "ak", time_value = as.Date("2020-01-01") + 1:200, value=1:200),
+  dplyr::tibble(geo_value = "al", time_value=as.Date("2020-01-01") + 1:5, value=-(1:5))
+) %>%
+  as_epi_df()
+
+f = function(x, ...) dplyr::tibble(value=mean(x$value), count=length(x$value))
+
 ## --- These cases generate the error: ---
 test_that("`ref_time_values` + `align` that result in no slide data, generate the error", {
   expect_error(edf %>% group_by(geo_value) %>% epi_slide(f, n=3L, ref_time_values=as.Date("2020-01-01")), 
