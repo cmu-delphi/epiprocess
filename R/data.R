e6b7c583989027a5bc8758d386b4e78bf60e7921
Daniel J. McDonald <dajmcdon@gmail.com>
2022-05-27 14:33:18 -0700

    remove ryantibs as code owner, some styling

--- R/data.R
+++ R/data.R
@@ -1,37 +1,53 @@
-#' Subset of JHU daily cases and deaths from California, Florida, Texas, New York, Georgia, and Pennsylvania
+#' Subset of JHU daily state cases and deaths 
 #'
-#' This data source of confirmed COVID-19 cases and deaths 
-#' is based on reports made available by the Center for 
-#' Systems Science and Engineering at Johns Hopkins University. 
-#' This example data ranges from Mar 1, 2020 to Dec 31, 2021, and is limited to California, Florida, Texas, New York, Georgia, and Pennsylvania.
+#' This data source of confirmed COVID-19 cases and deaths
+#' is based on reports made available by the Center for
+#' Systems Science and Engineering at Johns Hopkins University.
+#' This example data ranges from Mar 1, 2020 to Dec 31, 2021, and is limited to 
+#' California, Florida, Texas, New York, Georgia, and Pennsylvania.
 #'
 #' @format A tibble with 4026 rows and 6 variables:
 #' \describe{
-#'   \item{geo_value}{the geographic value associated with each row of measurements.}
+#'   \item{geo_value}{the geographic value associated with each row 
+#'       of measurements.}
 #'   \item{time_value}{the time value associated with each row of measurements.}
-#'   \item{case_rate_7d_av}{7-day average signal of number of new confirmed COVID-19 cases per 100,000 population, daily}
-#'   \item{death_rate_7d_av}{7-day average signal of number of new confirmed deaths due to COVID-19 per 100,000 population, daily}
+#'   \item{case_rate_7d_av}{7-day average signal of number of new 
+#'       confirmed COVID-19 cases per 100,000 population, daily}
+#'   \item{death_rate_7d_av}{7-day average signal of number of new confirmed 
+#'       deaths due to COVID-19 per 100,000 population, daily}
 #'   \item{cases}{Number of new confirmed COVID-19 cases, daily}
-#'   \item{cases_7d_av}{7-day average signal of number of new confirmed COVID-19 cases, daily}
+#'   \item{cases_7d_av}{7-day average signal of number of new confirmed 
+#'       COVID-19 cases, daily}
 #' }
-#' @source This object contains a modified part of the \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University} as \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{republished in the COVIDcast Epidata API}. This data set is licensed under the terms of the
-#' \href{https://creativecommons.org/licenses/by/4.0/}{Creative Commons Attribution 4.0 International license}
-#' by the Johns Hopkins University on behalf of its Center for Systems Science in Engineering.
-#' Copyright Johns Hopkins University 2020.
-#' 
+#' @source This object contains a modified part of the 
+#'   \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University}
+#'   as \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{republished in the COVIDcast Epidata API}. 
+#'   This data set is licensed under the terms of the
+#'   \href{https://creativecommons.org/licenses/by/4.0/}{Creative Commons Attribution 4.0 International license}
+#'   by the Johns Hopkins University on behalf of its Center for Systems Science
+#'   in Engineering. Copyright Johns Hopkins University 2020.
+#'
 #' Modifications:
-#' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{From the COVIDcast Epidata API}: These signals are taken directly from the JHU CSSE \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 GitHub repository} without changes. The 7-day average signals are computed by Delphi by calculating moving averages of the preceding 7 days, so the signal for June 7 is the average of the underlying data for June 1 through 7, inclusive.
-#' * Furthermore, the data has been limited to a very small number of rows, the signal names slightly altered, and formatted into a tibble.
+#' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{From the COVIDcast Epidata API}: 
+#'   These signals are taken directly from the JHU CSSE 
+#'   \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 GitHub repository} 
+#'   without changes. The 7-day average signals are computed by Delphi by 
+#'   calculating moving averages of the preceding 7 days, so the signal for 
+#'   June 7 is the average of the underlying data for June 1 through 7, 
+#'   inclusive.
+#' * Furthermore, the data has been limited to a very small number of rows, 
+#'   the signal names slightly altered, and formatted into a tibble.
 "jhu_csse_daily_subset"
 
 
-#' Subset of daily doctor visits and cases from California, Florida, Texas, and New York in archive format
+#' Subset of daily doctor visits and cases in archive format
 #'
-#' This data source is based on information about outpatient visits, 
-#' provided to us by health system partners, and also contains confirmed 
-#' COVID-19 cases based on reports made available by the Center for 
-#' Systems Science and Engineering at Johns Hopkins University. 
-#' This example data ranges from June 1, 2020 to Dec 1, 2021, and is also limited to California, Florida, Texas, and New York.
+#' This data source is based on information about outpatient visits,
+#' provided to us by health system partners, and also contains confirmed
+#' COVID-19 cases based on reports made available by the Center for
+#' Systems Science and Engineering at Johns Hopkins University.
+#' This example data ranges from June 1, 2020 to Dec 1, 2021, and 
+#' is also limited to California, Florida, Texas, and New York.
 #'
 #' @format An `epi_archive` data format. The data table DT has 129,638 rows and 5 columns:
 #' \describe{
@@ -41,12 +57,12 @@
 #'   \item{percent_cli}{percentage of doctor’s visits with CLI (COVID-like illness) computed from medical insurance claims}
 #'   \item{case_rate_7d_av}{7-day average signal of number of new confirmed deaths due to COVID-19 per 100,000 population, daily}
 #' }
-#' @source 
+#' @source
 #' This object contains a modified part of the \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University} as \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{republished in the COVIDcast Epidata API}. This data set is licensed under the terms of the
 #' \href{https://creativecommons.org/licenses/by/4.0/}{Creative Commons Attribution 4.0 International license}
 #' by Johns Hopkins University on behalf of its Center for Systems Science in Engineering.
 #' Copyright Johns Hopkins University 2020.
-#' 
+#'
 #' Modifications:
 #' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/doctor-visits.html}{From the COVIDcast Doctor Visits API}: The signal `percent_cli` is taken directly from the API without changes.
 #' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{From the COVIDcast Epidata API}: `case_rate_7d_av` is taken directly from the JHU CSSE \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 GitHub repository} without changes. The 7-day average signals are computed by Delphi by calculating moving averages of the preceding 7 days, so the signal for June 7 is the average of the underlying data for June 1 through 7, inclusive.
@@ -55,13 +71,14 @@
 
 
 #' Subset of JHU daily cases from California and Florida
-#' 
-#' This data source of confirmed COVID-19 cases 
-#' is based on reports made available by the Center for 
-#' Systems Science and Engineering at Johns Hopkins University. 
-#' This example data is a snapshot as of Oct 28, 2021 and captures the cases from June 1, 2020 to May 31, 2021 
+#'
+#' This data source of confirmed COVID-19 cases
+#' is based on reports made available by the Center for
+#' Systems Science and Engineering at Johns Hopkins University.
+#' This example data is a snapshot as of Oct 28, 2021 and captures the cases 
+#' from June 1, 2020 to May 31, 2021
 #' and is limited to California and Florida.
-#' 
+#'
 #' @format A tibble with 730 rows and 3 variables:
 #' \describe{
 #'   \item{geo_value}{the geographic value associated with each row of measurements.}
@@ -72,19 +89,20 @@
 #' \href{https://creativecommons.org/licenses/by/4.0/}{Creative Commons Attribution 4.0 International license}
 #' by the Johns Hopkins University on behalf of its Center for Systems Science in Engineering.
 #' Copyright Johns Hopkins University 2020.
-#' 
+#'
 #' Modifications:
-#' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{From the COVIDcast Epidata API}: 
-#' These signals are taken directly from the JHU CSSE \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 GitHub repository} without changes. 
+#' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{From the COVIDcast Epidata API}:
+#' These signals are taken directly from the JHU CSSE \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 GitHub repository} without changes.
 #' * Furthermore, the data has been limited to a very small number of rows, the signal names slightly altered, and formatted into a tibble.
 "incidence_num_outlier_example"
 
 #' Subset of JHU daily cases from counties in Massachusetts and Vermont
 #'
-#' This data source of confirmed COVID-19 cases and deaths 
-#' is based on reports made available by the Center for 
-#' Systems Science and Engineering at Johns Hopkins University. 
-#' This example data ranges from Mar 1, 2020 to Dec 31, 2021, and is limited to Massachusetts and Vermont.
+#' This data source of confirmed COVID-19 cases and deaths
+#' is based on reports made available by the Center for
+#' Systems Science and Engineering at Johns Hopkins University.
+#' This example data ranges from Mar 1, 2020 to Dec 31, 2021, 
+#' and is limited to Massachusetts and Vermont.
 #'
 #' @format A tibble with 16,212 rows and 5 variables:
 #' \describe{
@@ -98,9 +116,8 @@
 #' \href{https://creativecommons.org/licenses/by/4.0/}{Creative Commons Attribution 4.0 International license}
 #' by the Johns Hopkins University on behalf of its Center for Systems Science in Engineering.
 #' Copyright Johns Hopkins University 2020.
-#' 
+#'
 #' Modifications:
 #' * \href{https://cmu-delphi.github.io/delphi-epidata/api/covidcast-signals/jhu-csse.html}{From the COVIDcast Epidata API}: These signals are taken directly from the JHU CSSE \href{https://github.com/CSSEGISandData/COVID-19}{COVID-19 GitHub repository} without changes. The 7-day average signals are computed by Delphi by calculating moving averages of the preceding 7 days, so the signal for June 7 is the average of the underlying data for June 1 through 7, inclusive.
 #' * Furthermore, the data has been limited to a very small number of rows, the signal names slightly altered, and formatted into a tibble.
-
 "jhu_csse_county_level_subset"
