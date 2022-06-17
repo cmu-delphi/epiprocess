06f04eb41824f2672d4806fd4b46d122cdc7b6c4
kenmawer <kenmawer93@gmail.com>
2022-06-17 13:29:07 -0700

    Committed changes with updated documentation pulled from main, as well as updated tests.

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -1,3 +1,5 @@
-test_that("asdf",{
-  
+test_that("epi_cor throws errors as needed",{
+  expect_error(epi_cor(data.frame(x=1:10),1,1))
+  expect_error(epi_cor(archive_cases_dv_subset$DT,var2=1))
+  expect_error(epi_cor(archive_cases_dv_subset$DT,var1=1))
 })
