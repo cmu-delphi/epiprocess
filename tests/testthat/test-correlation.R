8fda8857be2c9a8266b2de56680995be3b66c171
kenmawer <kenmawer93@gmail.com>
2022-06-17 13:41:49 -0700

    Added shift test.

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -3,3 +3,8 @@ test_that("epi_cor throws errors as needed",{
   expect_error(epi_cor(archive_cases_dv_subset$DT,var2=1))
   expect_error(epi_cor(archive_cases_dv_subset$DT,var1=1))
 })
+
+test_that("shift works as it should",{
+  expect_identical(shift(1:100,1),dplyr::lead(1:100))
+  expect_identical(shift(1:100,-1),dplyr::lag(1:100))
+})
