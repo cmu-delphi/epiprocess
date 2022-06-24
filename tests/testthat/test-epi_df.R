6e634ad020ea6bae0b4317708eab8cc6eb155440
kenmawer <kenmawer93@gmail.com>
2022-06-24 14:36:55 -0700

    test-epi_df complete

--- tests/testthat/test-epi_df.R
+++ tests/testthat/test-epi_df.R
@@ -10,4 +10,13 @@ test_that("check that dataset contains time_value",{
   )
 })
 
+test_that("tsibble isn't preserved",{
+  df <- data.frame(geo_value = "ca",time_value = as.Date("2020-01-01"),cases=1)
+  my_tsibble <- as_tsibble(df,index=time_value)
+  my_epi_df <- as_epi_df(my_tsibble)
+  expect_false(inherits(my_epi_df,"tbl_ts"))
+})
 
+test_that("is_epi_df works as it should",{
+  expect_false(is_epi_df(data.frame(x=1)))
+})
