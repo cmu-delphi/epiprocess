0fa233a03ce4215d4766e80a7604ff5356438b48
kenmawer <kenmawer93@gmail.com>
2022-06-24 11:05:25 -0700

    A few tests.

--- tests/testthat/test-epi_df.R
+++ tests/testthat/test-epi_df.R
@@ -1,3 +1,13 @@
-test_that("x",{
-  
+test_that("check that dataset contains geo_value",{
+  expect_error(
+    as_epi_df(data.frame(x=1))
+  )
 })
+
+test_that("check that dataset contains time_value",{
+  expect_error(
+    as_epi_df(data.frame(geo_value=1))
+  )
+})
+
+
