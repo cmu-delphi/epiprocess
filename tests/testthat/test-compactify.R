929c3003114adeaf31bf82c440d86df0e44e2b88
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2022-06-30 14:52:13 -0700

    Clarify compactify testing code
    - Be explicit about `filter` -> `dplyr::filter`
    - Have `row_replace` fetch `dt` from its args, not global, making it resemble a
      dplyr verb more closely. Note in comment that this appears to use copy on write
      despite assigning into a `data.table` which sometimes uses reference semantics.

--- tests/testthat/test-compactify.R
+++ tests/testthat/test-compactify.R
@@ -6,19 +6,21 @@ dt <- archive_cases_dv$DT
 test_that("Input for compactify must be NULL or a boolean", {
   expect_error(as_epi_archive(dv_duplicated,compactify="no"))
 })
-  
-dt <- filter(dt,geo_value == "ca")
+
+dt <- dplyr::filter(dt, geo_value == "ca")
 dt$percent_cli <- c(1:80)
 dt$case_rate <- c(1:80)
 
-row_replace <- function(row,x,y) {
+row_replace <- function(dt,row,x,y) {
+  # (This way of "replacing" elements appears to use copy-on-write even though
+  # we are working with a data.table.)
   dt[row,4] <- x
   dt[row,5] <- y
   dt
 }
 
 # Rows 1 should not be eliminated even if NA
-dt <- row_replace(1,NA,NA) # Not LOCF
+dt <- row_replace(dt,1,NA,NA) # Not LOCF
 
 # NOTE! We are assuming that there are no NA's in geo_value, time_value,
 # and version. Even though compactify may erroneously remove the first row
@@ -26,25 +28,25 @@ dt <- row_replace(1,NA,NA) # Not LOCF
 # has problems beyond the scope of this test
 
 # Rows 11 and 12 correspond to different time_values
-dt <- row_replace(12,11,11) # Not LOCF
+dt <- row_replace(dt,12,11,11) # Not LOCF
 
 # Rows 20 and 21 only differ in version
-dt <- row_replace(21,20,20) # LOCF
+dt <- row_replace(dt,21,20,20) # LOCF
 
 # Rows 21 and 22 only differ in version
-dt <- row_replace(22,20,20) # LOCF
+dt <- row_replace(dt,22,20,20) # LOCF
 
 # Row 39 comprises the first NA's
-dt <-row_replace(39,NA,NA) # Not LOCF
+dt <- row_replace(dt,39,NA,NA) # Not LOCF
 
 # Row 40 has two NA's, just like its lag, row 39
-dt <- row_replace(40,NA,NA) # LOCF
+dt <- row_replace(dt,40,NA,NA) # LOCF
 
 # Row 62's values already exist in row 15, but row 15 is not a preceding row
-dt <- row_replace(62,15,15) # Not LOCF
+dt <- row_replace(dt,62,15,15) # Not LOCF
 
 # Row 73 only has one value carried over
-dt <- row_replace(74,73,74) # Not LOCF
+dt <- row_replace(dt,74,73,74) # Not LOCF
 
 dt_true <- as_tibble(as_epi_archive(dt,compactify=TRUE)$DT)
 dt_false <- as_tibble(as_epi_archive(dt,compactify=FALSE)$DT)
