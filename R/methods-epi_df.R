ff507ea1d7881369459fb68f2e274121465c3fc9
admin <admin@host70-185.resnet.ubc.ca>
2022-08-08 16:21:59 -0700

    Updated [.epi_df to address issues 192-194

--- R/methods-epi_df.R
+++ R/methods-epi_df.R
@@ -80,13 +80,38 @@ summary.epi_df = function(object, ...) {
   } 
   
   cn <- names(res)
-  nr <- vctrs::vec_size(x) 
-  not_epi_df <- (!("time_value" %in% cn) || !("geo_value" %in% cn) || vctrs::vec_size(res) > nr || any(i > nr))
   
-  if (not_epi_df) return(tibble::as_tibble(res))
+  # Duplicate key columns, Abort
+  dup_col_names = cn[duplicated(cn)]
+  if (length(dup_col_names) != 0) {
+    Abort(paste0("Column name(s) ", 
+                 paste(unique(dup_col_names), 
+                       collapse = ", "), " must not be duplicated."))
+  } 
   
-  # Use reclass as safeguard (in case class & metadata are dropped)
+  not_epi_df <- (!("time_value" %in% cn) || !("geo_value" %in% cn))
+  
+  if (not_epi_df) {
+    attributes(res)$metadata <- NULL 
+    return(tibble::as_tibble(res))
+  }
+  
+  # Use reclass as safeguard (in case class &/or metadata are dropped)
   reclass(res, attr(x, "metadata"))
+  
+  # Amend additional metadata if some other_keys cols are dropped
+  
+  # Extract the other_keys colnames in the original epi_df that do not occur in the subset
+  only_in_orig = setdiff(unlist(attr(x, "metadata")$other_keys), 
+                         cn[!(cn %in% c("geo_value", "time_value"))]) 
+  
+  if (length(only_orig) != 0) {
+    # remove all other_keys elements in the original epi_df that do not occur in subset
+    attr(res, "metadata")$other_keys = 
+      attr(res, "metadata")$other_keys[! attr(res, "metadata")$other_keys %in% only_in_orig]
+  }
+  
+  res
 }
 
 #' `dplyr` verbs
