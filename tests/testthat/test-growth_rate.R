4ce0fcd9be3898bfbcb7a66c032e8d59da99b487
kenmawer <kenmawer93@gmail.com>
2022-08-08 16:27:12 -0700

    I can't seem to figure out how to get the warning thrown. Setting x and y as c(1,1:19), for example, won't work.

--- tests/testthat/test-growth_rate.R
+++ tests/testthat/test-growth_rate.R
@@ -5,11 +5,10 @@ x <- jhu_csse_daily_subset %>%
   group_by(geo_value) %>% 
   mutate(cases_gr = growth_rate(x = time_value,  y = cases))
 
-test_that("`x` and `y` must be the same lengths",{
-  expect_error(growth_rate(x=1:3,y=1:4))
-})
-
-test_that("`x0` must be a subset of `x`",{
-  expect_error(growth_rate(x=1:20,y=1:20,x0=21))
+test_that("Test error throwing",{
+  expect_error(growth_rate(x=1:3,y=1:4),
+               "`x` and `y` must have the same length.")
+  expect_error(growth_rate(x=1:20,y=1:20,x0=21),
+               "`x0` must be a subset of `x`.")
   expect_error(growth_rate(x=1:20,y=1:20,x0=c(1,3)),NA)
 })
