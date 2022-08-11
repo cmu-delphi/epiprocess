6850a9af5e690c87fcb4c5012f26b1d2d3308f8b
kenmawer <kenmawer93@gmail.com>
2022-08-11 13:43:57 -0700

    Added more tests.

--- tests/testthat/test-growth_rate.R
+++ tests/testthat/test-growth_rate.R
@@ -28,13 +28,31 @@ test_that("Simple example of growth rate produces desired results",{
 })
 
 # Test each of the methods, log_scale settings, and na_rm settings
-growth_rate(x=1:20,y=2^(1:20),method="rel_change",h=1)
-growth_rate(x=1:20,y=2^(1:20),method="linear_reg",h=1)
-growth_rate(x=1:20,y=2^(1:20),method="smooth_spline",h=1)
-growth_rate(x=1:20,y=2^(1:20),method="trend_filter",h=1)
 
-growth_rate(x=1:20,y=sin(1:20)+1:20,h=4)
-growth_rate(x=1:20,y=sin(1:20)+1:20,method="rel_change",h=4)
-growth_rate(x=1:20,y=sin(1:20)+1:20,method="linear_reg",h=4)
-growth_rate(x=1:20,y=sin(1:20)+1:20,method="smooth_spline",h=4)
-growth_rate(x=1:20,y=sin(1:20)+1:20,method="trend_filter",h=4)
+test_that("Running different methods won't fail",{
+  expect_error(
+    for (m in c("rel_change","linear_reg","smooth_spline","trend_filter")) {
+      growth_rate(x=1:25,y=sin(0:24)+0:24+1,method=m,h=3)
+    },
+    NA
+  )
+})
+
+test_that("When using trend_filter, if `cv=FALSE`, then df must be an integer",{
+  expect_error(growth_rate(x=1:25,y=sin(0:24)+0:24+1,method="trend_filter",
+                           cv=FALSE,df=1.5,h=3),
+               "If `cv = FALSE`, then `df` must be an integer.")
+})
+
+growth_rate(x=1:20,y=exp(1:20),h=5,method="linear_reg",log_scale = TRUE)
+
+test_that("log_scale works",{
+  expect_equal(growth_rate(x=1:20,y=exp(1:20),h=5,method="linear_reg",log_scale = TRUE),
+               rep(1,20))
+})
+
+test_that("na_rm works",{
+  X <- c(1:10,NA,12:19,NA)
+  Y <- c(1:9,NA,NA,12:20)
+  growth_rate(x=X,y=Y,na_rm = TRUE)
+})
