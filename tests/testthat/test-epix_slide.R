9980ec2b2e62279c0e8c7d95f8bbc3b64fcb91a3
kenmawer <kenmawer93@gmail.com>
2022-08-18 11:17:44 -0700

    IDK what happened, but the refactoring went wrong.

--- tests/testthat/test-epix_slide.R
+++ tests/testthat/test-epix_slide.R
@@ -1,46 +1,41 @@
 library(dplyr)
 library(rlang)
 
-ea <- archive_cases_dv_subset$clone()
-
 test_that("epix_slide only works on an epi_archive",{
   expect_error(epix_slide(data.frame(x=1)))
 })
 
 test_that("epix_slide works as intended",{
-  x2 <- ea$clone()$DT %>%
-    filter(geo_value == "ca", version <= as.Date("2020-06-09")) %>%
-    select(-percent_cli,-case_rate_7d_av) %>%
-    mutate(binary = 2^(row_number())) %>%
+  x <- tibble::tribble(~version, ~time_value,
+                       5, c(1:2,4),
+                       6, c(1:2,4:5),
+                       7, 2:6) %>%
+    tidyr::unnest(time_value)
+  
+  xx <- bind_cols(geo_value = rep("x",12),
+                  arrange(x,time_value,version),
+                  binary = 2^(1:12)) %>%
     as_epi_archive()
   
-  versions <- seq(as.Date("2020-06-01"),
-                     as.Date("2020-06-09"),
-                     by = "1 day")
+  time_values <- 2:5
   
-  xx1 <- epix_slide(x = x2,
-                   f = ~ sum(.x$binary),
-                   before = 5,
-                   group_by = geo_value,
-                   ref_time_values = versions,
-                   new_col_name = 'sum_binary')
+  xx1 <- epix_slide(x = xx,
+                    f = ~ sum(.xx$binary),
+                    before = 3,
+                    group_by = geo_value,
+                    ref_time_values = versions,
+                    new_col_name = "sum_binary")
   
-  xx2 <- tibble(geo_value = rep("ca",7),
-               version = as.Date("2020-06-01") + 1:7,
-               sum_binary = c(2^1,
-                              2^6+2^1,
-                              2^11+2^6+2^1,
-                              2^16+2^11+2^6+2^1,
-                              2^19+2^16+2^12+2^7,
-                              2^21+2^19+2^16+2^13,
-                              2^22+2^21+2^19+2^17))
+  xx2 <- tibble(geo_value = rep("x",5),
+                time_value = as.Date("2020-06-01") + 1:5,
+                sum_binary = c(3))
   
   expect_identical(xx1,xx2) # *
   
-  xx3 <- x2$slide(f = ~ sum(.x$binary),
-                  before = 5,
+  xx3 <- xx$slide(f = ~ sum(.xx$binary),
+                  before = 3,
                   group_by = "geo_value",
-                  ref_time_values = versions,
+                  ref_time_values = time_values,
                   new_col_name = 'sum_binary')
   
   expect_identical(xx1,xx3) # This and * Imply xx2 and xx3 are identical
