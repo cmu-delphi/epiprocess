ecb36a37a52c6c399235a41833cde49c5a501722
kenmawer <kenmawer93@gmail.com>
2022-08-19 16:44:56 -0700

    Getting unknown error.

--- tests/testthat/test-epix_slide.R
+++ tests/testthat/test-epix_slide.R
@@ -6,12 +6,13 @@ test_that("epix_slide only works on an epi_archive",{
 
 test_that("epix_slide works as intended",{
   x <- tibble::tribble(~version, ~time_value, ~binary,
-                       5,       c(1:2,4),     2^(1:3),
-                       6,       c(1:2,4:5),   2^(4:7),
-                       7,       2:6,          2^(8:12)) %>%
+                       4,       c(1:3),       2^(1:3),
+                       5,       c(1:2,4),     2^(4:6),
+                       6,       c(1:2,4:5),   2^(7:10),
+                       7,       2:6,          2^(11:15)) %>%
     tidyr::unnest(c(time_value,binary))
   
-  xx <- bind_cols(geo_value = rep("x",12), x) %>%
+  xx <- bind_cols(geo_value = rep("x",15), x) %>%
     as_epi_archive()
   
   xx1 <- epix_slide(x = xx,
@@ -20,11 +21,12 @@ test_that("epix_slide works as intended",{
                     group_by = geo_value,
                     new_col_name = "sum_binary")
   
-  xx2 <- tibble(geo_value = rep("x",2),
+  xx2 <- tibble(geo_value = rep("x",3),
                 # 7 should also be there below; this is a bug on issue #153
-                time_value = c(5,6),
-                sum_binary = c(2^3,
-                               2^7+2^6)) %>%
+                time_value = c(4,5,6),
+                sum_binary = c(2^3+2^2,
+                               2^6+2^5,
+                               2^10+2^9)) %>%
     as_epi_df(as_of = 1) # Also a bug (issue #213)
   
   expect_identical(xx1,xx2) # *
