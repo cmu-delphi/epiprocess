d9682da0dc75494729c422dcba74a49d5663550c
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2022-08-23 03:08:45 -0700

    Fix some outdated docs, refine wording on others
    - Remove reference to `n` in slide vignette intro.
    - Update .Rd files with some older roxygen changes.
    - Fix "`time_value`s" -> "time steps" in `detect_outlr_rm` docs, and slightly
      tweak wording.

--- R/outliers.R
+++ R/outliers.R
@@ -128,11 +128,10 @@ detect_outlr = function(x = seq_along(y), y,
 #'   `y`).
 #' @param y Signal values.
 #' @param n Number of time steps to use in the rolling window. Default is 21.
-#'   This value is centrally aligned. When `n` is an odd number, the
-#'   rolling range goes between `(n-1)/2` `time_value`s before to `(n-1)/2`
-#'   `time_value`s after. When `n` is even, then  the
-#'   rolling range goes between `n/2-1` `time_value`s before to `n/2`
-#'   `time_value`s after.
+#'   This value is centrally aligned. When `n` is an odd number, the rolling
+#'   window extends from `(n-1)/2` time steps before each design point to `(n-1)/2`
+#'   time steps after. When `n` is even, then the rolling range extends from
+#'   `n/2-1` time steps before to `n/2` time steps after.
 #' @param log_transform Should a log transform be applied before running outlier
 #'   detection? Default is `FALSE`. If `TRUE`, and zeros are present, then the
 #'   log transform will be padded by 1.
