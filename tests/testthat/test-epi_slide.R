05c311f6394df8fa3699661f1652129db9ba4fe7
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2023-05-19 18:08:37 -0400

    tests

--- tests/testthat/test-epi_slide.R
+++ tests/testthat/test-epi_slide.R
@@ -9,6 +9,14 @@ grouped = dplyr::bind_rows(
   as_epi_df() %>%
   group_by(geo_value)
 
+small_x = dplyr::bind_rows(
+  dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value=11:15),
+  dplyr::tibble(geo_value = "al", time_value = d + 1:5, value=-(1:5))
+) %>%
+  as_epi_df(as_of = d + 6) %>%
+  group_by(geo_value)
+
+
 f = function(x, g, t) dplyr::tibble(value=mean(x$value), count=length(x$value))
 
 ## --- These cases generate errors (or not): ---
@@ -97,4 +105,124 @@ test_that("epi_slide alerts if the provided f doesn't take enough args", {
   expect_warning(epi_slide(grouped, f_x_dots, before = 1L, ref_time_values = d+1),
     regexp = "positional arguments before the `...` args",
     class = "check_sufficient_f_args__f_needs_min_args_before_dots")
+})
+
+test_that("basic epi_slide computation produces expected output", {
+  expected_output = dplyr::bind_rows(
+    dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value = 11:15, slide_value=cumsum(11:15)),
+    dplyr::tibble(geo_value = "al", time_value = d + 1:5, value = -(1:5), slide_value=cumsum(-(1:5)))
+  ) %>%
+    group_by(geo_value)
+  
+  # formula
+  result1 <- epi_slide(small_x, f = ~sum(.x$value), before=50)
+  expect_identical(result1, expected_output)
+
+  # function
+  result2 <- epi_slide(small_x, f = function(x, g, t) sum(x$value), before=50)
+  expect_identical(result2, expected_output)
+
+  # dots
+  result3 <- epi_slide(small_x, slide_value = sum(value), before=50)
+  expect_identical(result3, expected_output)
+})
+
+test_that("epi_slide computation via formula can use ref_time_value", {
+  expected_output = dplyr::bind_rows(
+    dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value = 11:15, slide_value=as.double(d + 1:5)),
+    dplyr::tibble(geo_value = "al", time_value = d + 1:5, value = -(1:5), slide_value=as.double(d + 1:5))
+  ) %>%
+    group_by(geo_value)
+
+  result1 <- small_x %>%
+    epi_slide(f = ~ .ref_time_value,
+               before = 50)
+
+  expect_identical(result1, expected_output)
+
+  result2 <- small_x %>%
+    epi_slide(f = ~ .z,
+               before = 50)
+
+  expect_identical(result2, expected_output)
+
+  result3 <- small_x %>%
+    epi_slide(f = ~ ..3,
+               before = 50)
+
+  expect_identical(result3, expected_output)
+})
+
+test_that("epi_slide computation via function can use ref_time_value", {
+  expected_output = dplyr::bind_rows(
+    dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value = 11:15, slide_value=as.double(d + 1:5)),
+    dplyr::tibble(geo_value = "al", time_value = d + 1:5, value = -(1:5), slide_value=as.double(d + 1:5))
+  ) %>%
+    group_by(geo_value)
+
+  result1 <- small_x %>%
+    epi_slide(f = function(x, g, t) t,
+               before = 2)
+
+  expect_identical(result1, expected_output)
+})
+
+test_that("epi_slide computation via dots can use ref_time_value and group", {
+  # ref_time_value
+  expected_output = dplyr::bind_rows(
+    dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value = 11:15, slide_value=as.double(d + 1:5)),
+    dplyr::tibble(geo_value = "al", time_value = d + 1:5, value = -(1:5), slide_value=as.double(d + 1:5))
+  ) %>%
+    group_by(geo_value)
+
+  result1 <- small_x %>%
+    epi_slide(before = 50,
+      slide_value = .ref_time_value)
+
+  expect_identical(result1, expected_output)
+
+  result2 <- small_x %>%
+    epi_slide(before = 50,
+      slide_value = .env$.ref_time_value)
+
+  expect_identical(result2, expected_output)
+
+  # group_key
+  # Use group_key column
+  expected_output = dplyr::bind_rows(
+    dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value = 11:15, slide_value="ak"),
+    dplyr::tibble(geo_value = "al", time_value = d + 1:5, value = -(1:5), slide_value="al")
+  ) %>%
+    group_by(geo_value)
+
+  result3 <- small_x %>%
+    epi_slide(before = 2,
+      slide_value = .group_key$geo_value)
+
+  expect_identical(result3, expected_output)
+
+  # Use entire group_key object
+  expected_output = dplyr::bind_rows(
+    dplyr::tibble(geo_value = "ak", time_value = d + 1:5, value = 11:15, slide_value=1L),
+    dplyr::tibble(geo_value = "al", time_value = d + 1:5, value = -(1:5), slide_value=1L)
+  ) %>%
+    group_by(geo_value)
+
+  result4 <- small_x %>%
+    epi_slide(before = 2,
+        slide_value = nrow(.group_key))
+
+  expect_identical(result4, expected_output)
+})
+
+test_that("epi_slide computation via dots outputs the same result using col names and the data var", {
+  expected_output <- small_x %>%
+    epi_slide(before = 2,
+      sum_binary = max(time_value))
+
+  result1 <- small_x %>%
+    epi_slide(before = 2,
+      sum_binary = max(.x$time_value))
+
+  expect_identical(result1, expected_output)
 })
