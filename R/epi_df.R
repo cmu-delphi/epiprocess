5886ec14f65982110cb788c4c34c487cebd23b9b
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2023-12-06 15:07:30 -0500

    make relocate input-col-order agnostic

--- R/epi_df.R
+++ R/epi_df.R
@@ -159,11 +159,13 @@ new_epi_df = function(x = tibble::tibble(), geo_type, time_type, as_of,
   
   # Reorder columns (geo_value, time_value, ...)
   if(sum(dim(x)) != 0){
-    cols_to_put_first <- colnames(x) %in% c("geo_value", "time_value")
+    cols_to_put_first <- c("geo_value", "time_value")
     x <- x[, c(
-      which(cols_to_put_first),
-      which(!cols_to_put_first)
-    )]
+      cols_to_put_first,
+      # All other columns
+      names(x)[!(names(x) %in% cols_to_put_first)]
+      )
+    ]
   }
   
   # Apply epi_df class, attach metadata, and return
