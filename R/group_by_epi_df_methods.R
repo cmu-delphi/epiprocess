0bf702f11cfd9ac13a65b7a1b62384937d38e33e
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2024-01-12 19:41:32 -0800

    Comment adding context for `group_by_epi_df_methods.R`

--- R/group_by_epi_df_methods.R
+++ R/group_by_epi_df_methods.R
@@ -1,3 +1,9 @@
+# These methods (and maybe some others in methods-epi_df.R) are here to augment
+# `?dplyr_extending` implementations to get the correct behavior on grouped
+# `epi_df`s. It would be nice if there were a way to replace these with a
+# generic core that automatically fixed all misbehaving methods; see
+# brainstorming within Issue #223.
+
 #' @importFrom dplyr select
 #' @export
 select.epi_df <- function(.data, ...) {
