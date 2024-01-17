949bf172b2fb068a4caa6e4a7faeb8598e9beaf2
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2024-01-17 18:48:54 -0500

    initial version of mean-specific epi_slide
    update examples

--- R/slide.R
+++ R/slide.R
@@ -373,3 +373,163 @@ epi_slide <- function(x, f, ..., before, after, ref_time_values,
 
   return(x)
 }
+
+#' Optimized slide function for performing rolling averages on an `epi_df` object
+#'
+#' Slides a n-timestep mean over variables in an `epi_df` object. See the [slide
+#' vignette](https://cmu-delphi.github.io/epiprocess/articles/slide.html) for
+#' examples.
+#'
+#' @param x The `epi_df` object under consideration, [grouped][dplyr::group_by]
+#'   or ungrouped. If ungrouped, all data in `x` will be treated as part of a
+#'   single data group.
+#' @param ... Additional arguments to pass to `data.table::frollmean`. `na.rm`
+#'   and `algo` are important to consider.
+#' @param before,after How far `before` and `after` each `ref_time_value` should
+#'   the sliding window extend? At least one of these two arguments must be
+#'   provided; the other's default will be 0. Any value provided for either
+#'   argument must be a single, non-`NA`, non-negative,
+#'   [integer-compatible][vctrs::vec_cast] number of time steps. Endpoints of
+#'   the window are inclusive. Common settings: * For trailing/right-aligned
+#'   windows from `ref_time_value - time_step(k)` to `ref_time_value`: either
+#'   pass `before=k` by itself, or pass `before=k, after=0`. * For
+#'   center-aligned windows from `ref_time_value - time_step(k)` to
+#'   `ref_time_value + time_step(k)`: pass `before=k, after=k`. * For
+#'   leading/left-aligned windows from `ref_time_value` to `ref_time_value +
+#'   time_step(k)`: either pass pass `after=k` by itself, or pass `before=0,
+#'   after=k`. See "Details:" about the definition of a time step,
+#'   (non)treatment of missing rows within the window, and avoiding warnings
+#'   about `before`&`after` settings for a certain uncommon use case.
+#' @param ref_time_values Time values for sliding computations, meaning, each
+#'   element of this vector serves as the reference time point for one sliding
+#'   window. If missing, then this will be set to all unique time values in the
+#'   underlying data table, by default.
+#' @param time_step Optional function used to define the meaning of one time
+#'   step, which if specified, overrides the default choice based on the
+#'   `time_value` column. This function must take a non-negative integer and
+#'   return an object of class `lubridate::period`. For example, we can use
+#'   `time_step = lubridate::hours` in order to set the time step to be one hour
+#'   (this would only be meaningful if `time_value` is of class `POSIXct`).
+#' @param new_col_name String indicating the name of the new column that will
+#'   contain the derivative values. Default is "slide_value"; note that setting
+#'   `new_col_name` equal to an existing column name will overwrite this column.
+#' @param as_list_col Should the slide results be held in a list column, or be
+#'   [unchopped][tidyr::unchop]/[unnested][tidyr::unnest]? Default is `FALSE`,
+#'   in which case a list object returned by `f` would be unnested (using
+#'   [`tidyr::unnest()`]), and, if the slide computations output data frames,
+#'   the names of the resulting columns are given by prepending `new_col_name`
+#'   to the names of the list elements.
+#' @param names_sep String specifying the separator to use in `tidyr::unnest()`
+#'   when `as_list_col = FALSE`. Default is "_". Using `NULL` drops the prefix
+#'   from `new_col_name` entirely.
+#' @param all_rows If `all_rows = TRUE`, then all rows of `x` will be kept in
+#'   the output even with `ref_time_values` provided, with some type of missing
+#'   value marker for the slide computation output column(s) for `time_value`s
+#'   outside `ref_time_values`; otherwise, there will be one row for each row in
+#'   `x` that had a `time_value` in `ref_time_values`. Default is `FALSE`. The
+#'   missing value marker is the result of `vctrs::vec_cast`ing `NA` to the type
+#'   of the slide computation output. If using `as_list_col = TRUE`, note that
+#'   the missing marker is a `NULL` entry in the list column; for certain
+#'   operations, you might want to replace these `NULL` entries with a different
+#'   `NA` marker.
+#' @return An `epi_df` object given by appending a new column to `x`, named
+#'   according to the `new_col_name` argument.
+#'
+#' @details To "slide" means to apply a function or formula over a rolling
+#'   window of time steps for each data group, where the window is entered at a
+#'   reference time and left and right endpoints are given by the `before` and
+#'   `after` arguments. The unit (the meaning of one time step) is implicitly
+#'   defined by the way the `time_value` column treats addition and subtraction;
+#'   for example, if the time values are coded as `Date` objects, then one time
+#'   step is one day, since `as.Date("2022-01-01") + 1` equals
+#'   `as.Date("2022-01-02")`. Alternatively, the time step can be set explicitly
+#'   using the `time_step` argument (which if specified would override the
+#'   default choice based on `time_value` column). If there are not enough time
+#'   steps available to complete the window at any given reference time, then
+#'   `epi_slide()` still attempts to perform the computation anyway (it does not
+#'   require a complete window). The issue of what to do with partial
+#'   computations (those run on incomplete windows) is therefore left up to the
+#'   user, either through the specified function or formula `f`, or through
+#'   post-processing. For a centrally-aligned slide of `n` `time_value`s in a
+#'   sliding window, set `before = (n-1)/2` and `after = (n-1)/2` when the
+#'   number of `time_value`s in a sliding window is odd and `before = n/2-1` and
+#'   `after = n/2` when `n` is even.
+#'
+#'   Sometimes, we want to experiment with various trailing or leading window
+#'   widths and compare the slide outputs. In the (uncommon) case where
+#'   zero-width windows are considered, manually pass both the `before` and
+#'   `after` arguments in order to prevent potential warnings. (E.g., `before=k`
+#'   with `k=0` and `after` missing may produce a warning. To avoid warnings,
+#'   use `before=k, after=0` instead; otherwise, it looks too much like a
+#'   leading window was intended, but the `after` argument was forgotten or
+#'   misspelled.)
+#'
+#' @importFrom lubridate days weeks
+#' @importFrom dplyr bind_rows group_vars filter select
+#' @importFrom rlang .data .env !! enquo enquos sym env missing_arg
+#' @importFrom data.table frollmean
+#' @export
+#' @examples
+#' # slide a 7-day trailing average formula on cases
+#' jhu_csse_daily_subset %>%
+#'   group_by(geo_value) %>%
+#'   epi_slide_mean(cases, new_col_name = cases_7dav, names_sep = NULL, before = 6) %>%
+#'   # rmv a nonessential var. to ensure new col is printed
+#'   dplyr::select(-death_rate_7d_av)
+#'
+#' # slide a 7-day leading average
+#' jhu_csse_daily_subset %>%
+#'   group_by(geo_value) %>%
+#'   epi_slide_mean(cases, new_col_name = cases_7dav, names_sep = NULL, after = 6) %>%
+#'   # rmv a nonessential var. to ensure new col is printed
+#'   dplyr::select(-death_rate_7d_av)
+#'
+#' # slide a 7-day centre-aligned average
+#' jhu_csse_daily_subset %>%
+#'   group_by(geo_value) %>%
+#'   epi_slide_mean(cases, new_col_name = cases_7dav, names_sep = NULL, before = 3, after = 3) %>%
+#'   # rmv a nonessential var. to ensure new col is printed
+#'   dplyr::select(-death_rate_7d_av)
+#'
+#' # slide a 14-day centre-aligned average
+#' jhu_csse_daily_subset %>%
+#'   group_by(geo_value) %>%
+#'   epi_slide_mean(cases, new_col_name = cases_7dav, names_sep = NULL, before = 6, after = 7) %>%
+#'   # rmv a nonessential var. to ensure new col is printed
+#'   dplyr::select(-death_rate_7d_av)
+epi_slide_mean = function(x, col_name, ..., before, after, ref_time_values,
+                     time_step,
+                     new_col_name = "slide_value", as_list_col = FALSE,
+                     names_sep = "_", all_rows = FALSE) {
+  all_dates <- seq(min(x$time_value), max(x$time_value), by = time_step)
+
+  ## TODO: need to deal with `after`
+  m <- before + 1L
+
+  if (is.null(names_sep)) {
+    result_col_name <- new_col_name
+  } else {
+    result_col_name <- paste(new_col_name, col_name, sep = names_sep)
+  }
+
+  result <- mutate(x, .real = TRUE) %>%
+    group_by(geo_value) %>%
+    group_modify(~{
+      pad_early_dates <- all_dates[1L] - (m - 1):1
+
+      # `setdiff` causes date formatting to change. Re-class these as dates.
+      missing_dates <- as.Date(setdiff(all_dates, .x$time_value), origin = "1970-01-01")
+      .x <- bind_rows(
+        .x,
+        tibble(time_value = c(pad_early_dates, missing_dates), .real = FALSE)
+      ) %>%
+        arrange(time_value)
+      .x[, c(result_col_name)] <- data.table::frollmean(.x[, c(col_name)], n = m, ...)
+      .x
+    }
+  )
+  result <- result[result$.real, ]
+  result$.real <- NULL
+  ungroup(result)
+}
+
