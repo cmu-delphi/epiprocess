7ec2d875f8caa5db662d535bca36466c8d3bac12
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2024-04-04 17:51:32 -0400

    outliers templates

---
+++ man-roxygen/x-y.R
@@ -0,0 +1,4 @@
+#' @param x Design points corresponding to the signal values `y`. Default is
+#'   `seq_along(y)` (that is, equally-spaced points from 1 to the length of
+#'   `y`).
+#' @param y Signal values.
