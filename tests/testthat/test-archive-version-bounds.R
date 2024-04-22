7bc4735d7df9e26ca11d76561d30e52e7950d392
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2024-04-22 15:55:47 -0700

    Fix version_bound_arg validation issues
    - Validate length.
    - Tweak message regarding type since typeof is length 1.
    - Actually raise error if NA when NA not allowed.
    - Make tests check the source of the error, since not being specific + R
      configuration masked some of these issues.

--- tests/testthat/test-archive-version-bounds.R
+++ tests/testthat/test-archive-version-bounds.R
@@ -2,17 +2,21 @@ test_that("`validate_version_bound` allows/catches `NA` as requested", {
   my_version_bound <- NA
   x <- tibble::tibble(version = 5L)
   validate_version_bound(my_version_bound, x, na_ok = TRUE)
-  expect_error(validate_version_bound(my_version_bound, x, na_ok = FALSE))
+  expect_error(validate_version_bound(my_version_bound, x, na_ok = FALSE),
+               class = "epiprocess__version_bound_na_with_na_not_okay")
 })
 
 test_that("`validate_version_bound` catches bounds that are the wrong length", {
   x <- tibble::tibble(version = 5L)
   my_version_bound1a <- NULL
-  expect_error(validate_version_bound(my_version_bound1a, x, na_ok = TRUE))
+  expect_error(validate_version_bound(my_version_bound1a, x, na_ok = TRUE),
+               class = "epiprocess__version_bound_null")
   my_version_bound1b <- integer(0L)
-  expect_error(validate_version_bound(my_version_bound1b, x, na_ok = TRUE))
+  expect_error(validate_version_bound(my_version_bound1b, x, na_ok = TRUE),
+               class = "epiprocess__version_bound_wrong_length")
   my_version_bound2 <- c(2, 10)
-  expect_error(validate_version_bound(my_version_bound2, x, na_ok = TRUE))
+  expect_error(validate_version_bound(my_version_bound2, x, na_ok = TRUE),
+               class = "epiprocess__version_bound_wrong_length")
 })
 
 test_that("`validate_version_bound` validate and class checks together allow and catch as intended", {
@@ -52,11 +56,13 @@ test_that("`validate_version_bound` validate and class checks together allow and
   validate_version_bound(my_datetime, x_datetime, version_bound_arg = "vb")
   expect_error(
     validate_version_bound(my_datetime, x_date, na_ok = TRUE, version_bound_arg = "vb"),
-    regexp = "must have the same classes"
+    regexp = "must have the same classes",
+    class = "epiprocess__version_bound_mismatched_class"
   )
   expect_error(
     validate_version_bound(my_date, x_datetime, na_ok = TRUE, version_bound_arg = "vb"),
-    regexp = "must have the same classes"
+    regexp = "must have the same classes",
+    class = "epiprocess__version_bound_mismatched_class"
   )
   # Bad:
   expect_error(validate_version_bound(3.5, x_int, TRUE, "vb"), regexp = "must have the same classes")
@@ -64,7 +70,7 @@ test_that("`validate_version_bound` validate and class checks together allow and
   expect_error(validate_version_bound(
     `class<-`(list(2), "clazz"),
     tibble::tibble(version = `class<-`(5L, "clazz")), TRUE, "vb"
-  ), regexp = "must have the same types")
+  ), regexp = "must have the same type", class = "epiprocess__version_bound_mismatched_typeof")
   # Maybe questionable:
   expect_error(validate_version_bound(3, x_int, TRUE, "vb"))
   expect_error(validate_version_bound(3L, x_dbl, TRUE, "vb"))
@@ -109,7 +115,8 @@ test_that("archive version bounds args work as intended", {
     ),
     regexp = "`clobberable_versions_start`.*indicated that there were later observed versions"
   )
-  expect_error(as_epi_archive(update_tbl, versions_end = NA), regexp = "must have the same classes")
+  expect_error(as_epi_archive(update_tbl, versions_end = NA),
+               class = "epiprocess__version_bound_na_with_na_not_okay")
   ea_default <- as_epi_archive(update_tbl)
   ea_default %>% epix_as_of(measurement_date + 4L)
   expect_warning(
