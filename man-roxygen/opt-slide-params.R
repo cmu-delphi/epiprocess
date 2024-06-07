de936f55ba705f350c92aaca83be6a4e03baa792
nmdefries <42820733+nmdefries@users.noreply.github.com>
2024-06-07 14:42:37 -0400

    tidyselect options description
    Co-authored-by: brookslogan <lcbrooks+github@andrew.cmu.edu>

--- man-roxygen/opt-slide-params.R
+++ man-roxygen/opt-slide-params.R
@@ -1,10 +1,13 @@
-#' @param col_names A character vector OR a
-#'  <[`tidy-select`][dplyr_tidy_select]> of the names of one or more columns
-#'  for which to calculate a rolling computation. If a tidy-selection, one
-#'  or more unquoted expressions separated by commas. Variable names can be
-#'  used as if they were positions in the data frame, so expressions like
-#'  `x:y` can be used to select a range of variables. The tidy-selection
-#'  cannot be used to provide output column names.
+#' @param col_names <[`tidy-select`][dplyr_tidy_select]> An unquoted column name
+#'   (e.g., `cases`), multiple column names (e.g., `c(cases, deaths)`), or other
+#'   tidy-select expression. Variable names can be used as if they were
+#'   positions in the data frame, so expressions like `x:y` can be used to
+#'   select a range of variables. If you have the desired column names stored in
+#'   a vector `vars`, use `col_names = all_of(vars)`.
+#'
+#'   The tidy-selection renaming interface is not supported, and cannot be used
+#'   to provide output column names; if you want to customize the output column
+#'   names, use [`dplyr::rename`] after the slide.
 #' @param as_list_col Not supported. Included to match `epi_slide` interface.
 #' @param new_col_name Character vector indicating the name(s) of the new
 #'  column(s) that will contain the derivative values. Default
