0763e990d1858b0dc19fe6db1b995fe25bf2664f
Daniel J. McDonald <dajmcdon@gmail.com>
2024-08-13 11:20:47 -0700

    styler

--- tests/testthat/test-arrange-canonical.R
+++ tests/testthat/test-arrange-canonical.R
@@ -4,12 +4,12 @@ test_that("canonical arrangement works", {
     y = rep(c("b", "b", "a", "a"), times = 2),
     geo_value = rep(c("ga", "ca"), each = 4),
     time_value = rep(2:1, times = 4)
-  ) 
+  )
   expect_warning(arrange_canonical(tib))
-  
+
   tib <- tib %>% as_epi_df(additional_metadata = list(other_keys = "y"))
   expect_equal(names(tib), c("geo_value", "time_value", "x", "y"))
-  
+
   tib_sorted <- arrange_canonical(tib)
   expect_equal(names(tib_sorted), c("geo_value", "time_value", "y", "x"))
   expect_equal(tib_sorted$geo_value, rep(c("ca", "ga"), each = 4))
