46061fb8ef71fbb7382ab9218325925daca1a01b
Daniel McDonald <dajmcdon@gmail.com>
2024-08-15 10:48:51 -0700

    Update tests/testthat/test-utils.R
    Co-authored-by: brookslogan <lcbrooks+github@andrew.cmu.edu>

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -251,8 +251,8 @@ test_that("guess_period works", {
     weekly_posixcts
   )
   # On POSIXlts:
-  daily_posixlts <- as.POSIXlt(daily_dates, tz = "UTC") + 3600
-  weekly_posixlts <- as.POSIXlt(weekly_dates, tz = "UTC") + 3600
+  daily_posixlts <- as.POSIXlt(daily_dates, tz = "US/Aleutian") + 3600
+  weekly_posixlts <- as.POSIXlt(weekly_dates, tz = "US/Aleutian") + 3600
   expect_identical(
     daily_posixlts[[1L]] + guess_period(daily_posixlts) * (seq_along(daily_posixlts) - 1L),
     daily_posixlts
