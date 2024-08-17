56f4988aebb1017e4c6aab33bfcccc888cdb3d13
Ryan Nayebi <32079381+rnayebi21@users.noreply.github.com>
2024-08-16 22:05:00 -0700

    Update R/methods-epi_archive.R
    Co-authored-by: Dmitry Shemetov <dshemetov@ucdavis.edu>

--- R/methods-epi_archive.R
+++ R/methods-epi_archive.R
@@ -270,7 +270,7 @@ epix_fill_through_version <- function(x, fill_versions_end,
 #'
 #' # Example 2
 #' # The s1 signal at August 1st gets revised from 12 to 13 on August 3rd
-#' s1 <- tibble(
+#' s1 <- tibble::tibble(
 #'   geo_value = c("ca", "ca", "ca", "ca"),
 #'   time_value = as.Date(c("2024-08-01", "2024-08-01", "2024-08-02", "2024-08-03")),
 #'   version = as.Date(c("2024-08-01", "2024-08-03", "2024-08-03", "2024-08-03")),
