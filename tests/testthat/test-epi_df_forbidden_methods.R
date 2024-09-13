fd044338b622d9a56bf9e2e688d434e9e4434744
brookslogan <brookslogan@users.noreply.github.com>
2024-09-13 19:15:50 +0000

    style: styler (GHA)

--- tests/testthat/test-epi_df_forbidden_methods.R
+++ tests/testthat/test-epi_df_forbidden_methods.R
@@ -1,4 +1,3 @@
-
 edf <- as_epi_df(tibble(
   geo_value = rep("nd", 10L),
   time_value = as.Date("2020-01-01") + 1:10 - 1L,
@@ -8,9 +7,11 @@ edf <- as_epi_df(tibble(
 test_that("Forbidden epi_df methods catches omitted column names in slide comp", {
   for (f in list(mean, sum, prod, min, max, all, any, range)) {
     expect_error(edf %>% epi_slide(.window_size = 7L, ~ f(.x)),
-                 class = "epiprocess__summarizer_on_entire_epi_df")
+      class = "epiprocess__summarizer_on_entire_epi_df"
+    )
     expect_error(edf %>% group_by(geo_value) %>% epi_slide(.window_size = 7L, ~ f(.x)),
-                 class = "epiprocess__summarizer_on_entire_epi_df")
+      class = "epiprocess__summarizer_on_entire_epi_df"
+    )
   }
 })
 
