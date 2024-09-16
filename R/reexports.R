d88d44d4b9af91ca8a44a8cf0f6c2623f01d1cda
nmdefries <nmdefries@users.noreply.github.com>
2024-09-16 20:58:10 +0000

    style: styler (GHA)

--- R/reexports.R
+++ R/reexports.R
@@ -82,108 +82,108 @@ ggplot2::autoplot
 #' @inherit epidatasets::cases_deaths_subset description source references title
 #' @inheritSection epidatasets::cases_deaths_subset Data dictionary
 #' @examples
-#'  # Since this is a re-exported dataset, it cannot be loaded using
-#'  # the `data()` function. `data()` looks for a file of the same name
-#'  # in the `data/` directory, which doesn't exist in this package.
-#'  # works
-#'  epiprocess::cases_deaths_subset
+#' # Since this is a re-exported dataset, it cannot be loaded using
+#' # the `data()` function. `data()` looks for a file of the same name
+#' # in the `data/` directory, which doesn't exist in this package.
+#' # works
+#' epiprocess::cases_deaths_subset
 #'
-#'  # works
-#'  library(epiprocess)
-#'  cases_deaths_subset
+#' # works
+#' library(epiprocess)
+#' cases_deaths_subset
 #'
-#'  # fails
-#'  data(cases_deaths_subset, package = "epiprocess")
+#' # fails
+#' data(cases_deaths_subset, package = "epiprocess")
 #' @export
 delayedAssign("cases_deaths_subset", epidatasets::cases_deaths_subset)
 
 #' @inherit epidatasets::covid_incidence_county_subset description source references title
 #' @inheritSection epidatasets::covid_incidence_county_subset Data dictionary
 #' @examples
-#'  # Since this is a re-exported dataset, it cannot be loaded using
-#'  # the `data()` function. `data()` looks for a file of the same name
-#'  # in the `data/` directory, which doesn't exist in this package.
-#'  # works
-#'  epiprocess::covid_incidence_county_subset
+#' # Since this is a re-exported dataset, it cannot be loaded using
+#' # the `data()` function. `data()` looks for a file of the same name
+#' # in the `data/` directory, which doesn't exist in this package.
+#' # works
+#' epiprocess::covid_incidence_county_subset
 #'
-#'  # works
-#'  library(epiprocess)
-#'  covid_incidence_county_subset
+#' # works
+#' library(epiprocess)
+#' covid_incidence_county_subset
 #'
-#'  # fails
-#'  data(covid_incidence_county_subset, package = "epiprocess")
+#' # fails
+#' data(covid_incidence_county_subset, package = "epiprocess")
 #' @export
 delayedAssign("covid_incidence_county_subset", epidatasets::covid_incidence_county_subset)
 
 #' @inherit epidatasets::covid_incidence_outliers description source references title
 #' @inheritSection epidatasets::covid_incidence_outliers Data dictionary
 #' @examples
-#'  # Since this is a re-exported dataset, it cannot be loaded using
-#'  # the `data()` function. `data()` looks for a file of the same name
-#'  # in the `data/` directory, which doesn't exist in this package.
-#'  # works
-#'  epiprocess::covid_incidence_outliers
+#' # Since this is a re-exported dataset, it cannot be loaded using
+#' # the `data()` function. `data()` looks for a file of the same name
+#' # in the `data/` directory, which doesn't exist in this package.
+#' # works
+#' epiprocess::covid_incidence_outliers
 #'
-#'  # works
-#'  library(epiprocess)
-#'  covid_incidence_outliers
+#' # works
+#' library(epiprocess)
+#' covid_incidence_outliers
 #'
-#'  # fails
-#'  data(covid_incidence_outliers, package = "epiprocess")
+#' # fails
+#' data(covid_incidence_outliers, package = "epiprocess")
 #' @export
 delayedAssign("covid_incidence_outliers", epidatasets::covid_incidence_outliers)
 
 #' @inherit epidatasets::jhu_confirmed_cumulative_num description source references title
 #' @inheritSection epidatasets::jhu_confirmed_cumulative_num Data dictionary
 #' @examples
-#'  # Since this is a re-exported dataset, it cannot be loaded using
-#'  # the `data()` function. `data()` looks for a file of the same name
-#'  # in the `data/` directory, which doesn't exist in this package.
-#'  # works
-#'  epiprocess::jhu_confirmed_cumulative_num
+#' # Since this is a re-exported dataset, it cannot be loaded using
+#' # the `data()` function. `data()` looks for a file of the same name
+#' # in the `data/` directory, which doesn't exist in this package.
+#' # works
+#' epiprocess::jhu_confirmed_cumulative_num
 #'
-#'  # works
-#'  library(epiprocess)
-#'  jhu_confirmed_cumulative_num
+#' # works
+#' library(epiprocess)
+#' jhu_confirmed_cumulative_num
 #'
-#'  # fails
-#'  data(jhu_confirmed_cumulative_num, package = "epiprocess")
+#' # fails
+#' data(jhu_confirmed_cumulative_num, package = "epiprocess")
 #' @export
 delayedAssign("jhu_confirmed_cumulative_num", epidatasets::jhu_confirmed_cumulative_num)
 
 #' @inherit epidatasets::covid_case_death_rates description source references title
 #' @inheritSection epidatasets::covid_case_death_rates Data dictionary
 #' @examples
-#'  # Since this is a re-exported dataset, it cannot be loaded using
-#'  # the `data()` function. `data()` looks for a file of the same name
-#'  # in the `data/` directory, which doesn't exist in this package.
-#'  # works
-#'  epiprocess::covid_case_death_rates
+#' # Since this is a re-exported dataset, it cannot be loaded using
+#' # the `data()` function. `data()` looks for a file of the same name
+#' # in the `data/` directory, which doesn't exist in this package.
+#' # works
+#' epiprocess::covid_case_death_rates
 #'
-#'  # works
-#'  library(epiprocess)
-#'  covid_case_death_rates
+#' # works
+#' library(epiprocess)
+#' covid_case_death_rates
 #'
-#'  # fails
-#'  data(covid_case_death_rates, package = "epiprocess")
+#' # fails
+#' data(covid_case_death_rates, package = "epiprocess")
 #' @export
 delayedAssign("covid_case_death_rates", epidatasets::covid_case_death_rates)
 
 #' @inherit epidatasets::archive_cases_dv_subset description source references title
 #' @inheritSection epidatasets::archive_cases_dv_subset Data dictionary
 #' @examples
-#'  # Since this is a re-exported dataset, it cannot be loaded using
-#'  # the `data()` function. `data()` looks for a file of the same name
-#'  # in the `data/` directory, which doesn't exist in this package.
-#'  # works
-#'  epiprocess::archive_cases_dv_subset
+#' # Since this is a re-exported dataset, it cannot be loaded using
+#' # the `data()` function. `data()` looks for a file of the same name
+#' # in the `data/` directory, which doesn't exist in this package.
+#' # works
+#' epiprocess::archive_cases_dv_subset
 #'
-#'  # works
-#'  library(epiprocess)
-#'  archive_cases_dv_subset
+#' # works
+#' library(epiprocess)
+#' archive_cases_dv_subset
 #'
-#'  # fails
-#'  data(archive_cases_dv_subset, package = "epiprocess")
+#' # fails
+#' data(archive_cases_dv_subset, package = "epiprocess")
 #'
 #' @export
 delayedAssign("archive_cases_dv_subset", epidatasets::archive_cases_dv_subset)
