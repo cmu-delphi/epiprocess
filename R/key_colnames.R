f77d14caa36c12e2637817b946a746c52341cb42
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2024-10-09 17:26:41 -0700

    Make `key_colnames` map to unique key + more demanding
    * Make `key_colnames.epi_archive` output epikey-time-version rather than just
      epikey-time.
    * Make `key_colnames.data.frame` require `other_keys` be provided.
    * Remove `key_colnames.default`.
    * Make `key_colnames` forbid passing `exclude` positionally.
    * Update downstream `revision_summary`.

--- R/key_colnames.R
+++ R/key_colnames.R
@@ -1,28 +1,29 @@
-#' Grab any keys associated to an epi_df
+#' Get names of columns that form a (unique) key associated with an object
 #'
-#' @param x a data.frame, tibble, or epi_df
+#' This is entirely based on metadata and arguments passed; there are no
+#' explicit checks that the key actually is unique in any associated data
+#' structures.
+#'
+#' @param x an object, such as an [`epi_df`]
 #' @param ... additional arguments passed on to methods
-#' @param other_keys an optional character vector of other keys to include
-#' @param exclude an optional character vector of keys to exclude
-#' @return If an `epi_df`, this returns all "keys". Otherwise `NULL`.
+#' @param other_keys character vector; what besides `geo_value` and `time_value`
+#'   (if present) should we consider to be key columns?  Used, e.g., if we 
+#' @param exclude an optional character vector of key column names to exclude
+#'   from the result
+#' @return character vector
 #' @keywords internal
 #' @export
-key_colnames <- function(x, ...) {
+key_colnames <- function(x, ..., exclude = character(0L)) {
   UseMethod("key_colnames")
 }
 
 #' @rdname key_colnames
-#' @method key_colnames default
-#' @export
-key_colnames.default <- function(x, ...) {
-  character(0L)
-}
-
-#' @rdname key_colnames
+#' @importFrom rlang check_dots_empty0
 #' @method key_colnames data.frame
 #' @export
-key_colnames.data.frame <- function(x, other_keys = character(0L), exclude = character(0L), ...) {
+key_colnames.data.frame <- function(x, other_keys, ..., exclude = character(0L)) {
   assert_character(other_keys)
+  check_dots_empty0(...)
   assert_character(exclude)
   nm <- setdiff(c("geo_value", other_keys, "time_value"), exclude)
   intersect(nm, colnames(x))
@@ -31,7 +32,8 @@ key_colnames.data.frame <- function(x, other_keys = character(0L), exclude = cha
 #' @rdname key_colnames
 #' @method key_colnames epi_df
 #' @export
-key_colnames.epi_df <- function(x, exclude = character(0L), ...) {
+key_colnames.epi_df <- function(x, ..., exclude = character(0L)) {
+  check_dots_empty0(...)
   assert_character(exclude)
   other_keys <- attr(x, "metadata")$other_keys
   setdiff(c("geo_value", other_keys, "time_value"), exclude)
@@ -40,8 +42,8 @@ key_colnames.epi_df <- function(x, exclude = character(0L), ...) {
 #' @rdname key_colnames
 #' @method key_colnames epi_archive
 #' @export
-key_colnames.epi_archive <- function(x, exclude = character(0L), ...) {
+key_colnames.epi_archive <- function(x, ..., exclude = character(0L)) {
+  check_dots_empty0(...)
   assert_character(exclude)
-  other_keys <- attr(x, "metadata")$other_keys
-  setdiff(c("geo_value", other_keys, "time_value"), exclude)
+  setdiff(c("geo_value", x$other_keys, "time_value", "version"), exclude)
 }
