9effa5cfe0033ca72ce4aebc95ebe82139d0a213
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2025-02-26 13:03:31 -0800

    feat: WIP epix_epi_slide_opt

---
+++ R/epix_epi_slide_opt.R
@@ -0,0 +1,143 @@
+epix_epi_slide_opt_one_epikey <- function(updates, in_colnames, f, f_from_package, before, after, time_type, out_colnames) {
+  if (before == Inf) {
+    cli_abort("epiprocess internal error: epix_epi_slide_opt_one_epikey() called with before == Inf")
+  }
+  unit_step <- epiprocess:::unit_time_delta(time_type)
+  prev_inp_snapshot <- NULL
+  prev_out_snapshot <- NULL
+  result <- map(seq_len(nrow(updates)), function(update_i) {
+    version <- updates$version[[update_i]]
+    inp_update <- updates$subtbl[[update_i]] # TODO decide whether DT
+    ## setDF(inp_update)
+    ## inp_update <- new_tibble(inp_update, nrow = nrow(inp_update))
+    inp_snapshot <- tbl_patch(prev_inp_snapshot, inp_update, "time_value")
+    inp_update_min_t <- min(inp_update$time_value) # TODO check efficiency
+    inp_update_max_t <- max(inp_update$time_value)
+    # If the input had updates in the range t1..t2, this could produce changes
+    # in slide outputs in the range t1-after..t2+before, and to compute those
+    # slide values, we need to look at the input snapshot from
+    # t1-after-before..t2+before+after.
+    slide_min_t <- inp_update_min_t - (before + after) * unit_step
+    slide_max_t <- inp_update_max_t + (before + after) * unit_step
+    slide_n <- time_delta_to_n_steps(slide_max_t - slide_min_t, time_type) + 1L
+    slide_time_values <- slide_min_t + 0L:(slide_n - 1L) * unit_step
+    slide_inp_backrefs <- vec_match(slide_time_values, inp_snapshot$time_value)
+    slide <- inp_snapshot[slide_inp_backrefs, ] # TODO vs. DT key index vs ....
+    slide$time_value <- slide_time_values
+    # TODO ensure before & after as integers?
+    # TODO parameterize naming, slide function, options, ...
+    if (f_from_package == "data.table") {
+      for (col_i in seq_along(in_colnames)) {
+        # FIXME other arg forwarding
+        slide[[out_colnames[[col_i]]]] <- f(slide[[in_colnames[[col_i]]]], before + after + 1L)
+      }
+    } else if (f_from_package == "slider") {
+      for (col_i in seq_along(in_colnames)) {
+        # with adaptive tails that incorporate fewer inputs:
+        # FIXME other arg forwarding
+        out_col <- f(slide[[in_colnames[[col_i]]]], before = before, after = after)
+        # XXX is this actually required or being done at the right time? we are
+        # already chopping off a good amount that might include this?
+        #
+        # FIXME can this generate an error on very short series?
+        vec_slice(out_col, seq_len(before)) <- NA
+        vec_slice(out_col, slide_n - after + seq_len(after)) <- NA
+        slide[[out_colnames[[col_i]]]] <- out_col
+      }
+    } else {
+      cli_abort("epiprocess internal error: `f_from_package` was {format_chr_deparse(f_from_package)}")
+    }
+    out_update <- slide[
+      # Get back to t1-after..t2+before; times outside this range were included
+      # only so those inside would have enough context for their slide
+      # computations, but these "context" rows may contain invalid slide
+      # computation outputs:
+      vec_rep_each(c(FALSE, TRUE, FALSE), c(before, slide_n - before - after, after)) &
+        # Only include time_values that appeared in the input snapshot:
+        !is.na(slide_inp_backrefs),
+    ]
+    out_diff <- tbl_diff2(prev_out_snapshot, out_update, "time_value", "update")
+    out_snapshot <- tbl_patch(prev_out_snapshot, out_diff)
+    prev_inp_snapshot <<- inp_snapshot
+    prev_out_snapshot <<- out_snapshot # TODO avoid need to patch twice?
+    out_diff$version <- version
+    out_diff
+  })
+  result
+}
+
+#' @export
+epix_epi_slide_opt <-
+  function(.x, .col_names, .f, ...,
+           .window_size = NULL, .align = c("right", "center", "left"),
+           .prefix = NULL, .suffix = NULL, .new_col_names = NULL # ,
+           ## .ref_time_values = NULL, .all_rows = FALSE
+  ) {
+    UseMethod("epix_epi_slide_opt")
+  }
+
+#' @method epix_epi_slide_opt grouped_epi_archive
+#' @export
+epix_epi_slide_opt.grouped_epi_archive <- function(.x, ...) {
+  assert_set_equal(
+    group_vars(.x),
+    key_colnames(.x, exclude = c("time_value", "version"))
+  )
+  orig_group_vars <- group_vars(.x)
+  orig_drop <- .x$private$drop
+  .x %>%
+    ungroup() %>%
+    epix_epi_slide_opt(...) %>%
+    group_by(pick(all_of(orig_group_vars)), .drop = orig_drop)
+}
+
+#' @method epix_epi_slide_opt epi_archive
+#' @export
+epix_epi_slide_opt.epi_archive <-
+  function(.x, .col_names, .f, ...,
+           .window_size = NULL, .align = c("right", "center", "left"),
+           .prefix = NULL, .suffix = NULL, .new_col_names = NULL,
+           ## , .ref_time_values = NULL, .all_rows = FALSE
+           .progress = FALSE) {
+    # Extract metadata:
+    time_type <- .x$time_type
+    epikey_names <- key_colnames(.x, exclude = c("time_value", "version"))
+    # Validation & pre-processing:
+    .align <- arg_match(.align)
+    f_info <- upstream_slide_f_info(.f)
+    col_names_quo <- enquo(.col_names)
+    names_info <- across_ish_names_info(.x$DT, time_type, col_names_quo, f_info$namer, .window_size, .align, .prefix, .suffix, .new_col_names)
+    window_args <- get_before_after_from_window(.window_size, .align, time_type)
+    assert(
+      checkmate::check_logical(.progress, any.missing = FALSE, len = 1L, names = "unnamed"),
+      checkmate::check_string(.progress)
+    )
+    if (isTRUE(.progress)) {
+      .progress <- "Time series processed:"
+    }
+    use_progress <- !isFALSE(.progress)
+    # Perform the slide:
+    updates_grouped <- .x$DT %>%
+      as.data.frame() %>%
+      as_tibble(.name_repair = "minimal") %>%
+      # 0 rows input -> 0 rows output, so we can just say drop = TRUE:
+      grouped_df(epikey_names, TRUE)
+    if (use_progress) progress_bar_id <- cli::cli_progress_bar(.progress, total = n_groups(updates_grouped))
+    result <- updates_grouped %>%
+      group_modify(function(group_values, group_key) {
+        group_updates <- group_values %>%
+          nest(.by = version, .key = "subtbl") %>%
+          arrange(version)
+        res <- epix_epi_slide_opt_one_epikey(group_updates, names_info$input_col_names, .f, f_info$from_package, window_args$before, window_args$after, time_type, names_info$output_col_names) %>%
+          list_rbind()
+        if (use_progress) cli::cli_progress_update(id = progress_bar_id)
+        res
+      }) %>%
+      ungroup() %>%
+      new_epi_archive(
+        .x$geo_type, .x$time_type, .x$other_keys,
+        .x$clobberable_versions_start, .x$versions_end
+      )
+    if (use_progress) cli::cli_progress_done(id = progress_bar_id)
+    result
+  }
---
+++ R/patch.R
@@ -0,0 +1,236 @@
+# TODO use these in apply_compactify
+approx_equal <- function(vec1, vec2, abs_tol, na_equal, .ptype = NULL, recurse = approx_equal, inds1 = NULL, inds2 = NULL) {
+  vecs <- list(vec1, vec2)
+  if (!is.null(inds1)) {
+    # XXX could have logical or integerish inds; just leave it to later checks
+    # to hopefully abort for now
+  } else {
+    vecs <- vec_recycle_common(!!!vecs)
+  }
+  vecs <- vec_cast_common(!!!vecs, .to = .ptype)
+  approx_equal0(vecs[[1]], vecs[[2]], abs_tol, na_equal, rec = approx_equal, inds1, inds2)
+}
+
+approx_equal0 <- function(vec1, vec2, abs_tol, na_equal, recurse = approx_equal0, inds1 = NULL, inds2 = NULL) {
+  if (is_bare_numeric(vec1) && abs_tol != 0) {
+    if (!is.null(inds1)) {
+      vec1 <- vec1[inds1]
+      vec2 <- vec2[inds2]
+    }
+    # perf: since we're working with bare numerics and logicals: we can use
+    # fifelse, and stop it from propagating attributes when there's no special
+    # class to guide the meaning
+    res <- fifelse(
+      !is.na(vec1) & !is.na(vec2),
+      abs(vec1 - vec2) <= abs_tol,
+      if (na_equal) is.na(vec1) & is.na(vec2) else FALSE
+    )
+    attributes(res) <- NULL
+    return(res)
+  } else if (is.data.frame(vec1) && abs_tol != 0) {
+    # (we only need to manually recurse if we potentially have columns that would
+    # be specially processed by the above)
+    if (ncol(vec1) == 0) {
+      rep(TRUE, nrow(vec1))
+    } else {
+      Reduce(`&`, lapply(seq_len(ncol(vec1)), function(col_i) {
+        recurse(vec1[[col_i]], vec2[[col_i]], abs_tol, na_equal, recurse, inds1, inds2)
+      }))
+    }
+  } else {
+    # No special handling for any other types/situations. Makes sense for
+    # unclassed atomic things; bare lists and certain vctrs classes might want
+    # recursion / specialization, though.
+    if (!is.null(inds1)) {
+      vec1 <- vec_slice(vec1, inds1)
+      vec2 <- vec_slice(vec2, inds2)
+    }
+    res <- vec_equal(vec1, vec2, na_equal = na_equal)
+    return(res)
+  }
+}
+
+#' Calculate compact patch to move from one snapshot/update to another
+#'
+#' @param earlier_snapshot tibble or `NULL`; `NULL` represents that there was no
+#'   data before `later_tbl`.
+#' @param later_tbl tibble; must have the same column names as
+#'   `earlier_snapshot` if it is a tibble.
+#' @param ukey_names character; column names that together, form a unique key
+#'   for `earlier_snapshot` and for `later_tbl`. This is unchecked; see
+#'   [`check_ukey_unique`] if you don't already have this guaranteed.
+#' @param later_format "snapshot" or "update"; default is "snapshot". If
+#'   "snapshot", `later_tbl` will be interpreted as a full snapshot of the data
+#'   set including all ukeys, and any ukeys that are in `earlier_snapshot` but
+#'   not in `later_tbl` are interpreted as deletions, which are currently
+#'   (imprecisely) represented in the output patch as revisions of all
+#'   non-`ukey_names` columns to NA values (using `{vctrs}`). If "update", then
+#'   it's assumed that any deletions have already been represented this way in
+#'   `later_tbl` and any ukeys not in `later_tbl` are simply unchanged; we are
+#'   just ensuring that the update is fully compact for the given
+#'   `compactify_abs_tol`.
+#' @param compactify_abs_tol compactification tolerance; see `apply_compactify`
+#' @return a tibble in compact "update" (diff) format
+tbl_diff2 <- function(earlier_snapshot, later_tbl,
+                      ukey_names,
+                      later_format = c("snapshot", "update"),
+                      compactify_abs_tol = 0) {
+  # Most input validation + handle NULL earlier_snapshot. This is a small function so
+  # use faster validation variants:
+  if (!is_tibble(later_tbl)) {
+    cli_abort("`later_tbl` must be a tibble")
+  }
+  if (is.null(earlier_snapshot)) {
+    return(later_tbl)
+  }
+  if (!is_tibble(earlier_snapshot)) {
+    cli_abort("`earlier_snapshot` must be a tibble or `NULL`")
+  }
+  later_format <- arg_match0(later_format, c("snapshot", "update"))
+  if (!(is.vector(compactify_abs_tol, mode = "numeric") && length(compactify_abs_tol) == 1L && compactify_abs_tol >= 0)) {
+    # Give a specific message:
+    assert_numeric(compactify_abs_tol, lower = 0, any.missing = FALSE, len = 1L)
+    # Fallback e.g. for invalid classes not caught by assert_numeric:
+    cli_abort("`compactify_abs_tol` must be a length-1 double/integer >= 0")
+  }
+
+  # Extract metadata:
+  earlier_n <- nrow(earlier_snapshot)
+  later_n <- nrow(later_tbl)
+  tbl_names <- names(earlier_snapshot)
+  val_names <- tbl_names[!tbl_names %in% ukey_names]
+
+  # More input validation:
+  if (!identical(tbl_names, names(later_tbl))) {
+    # XXX is this check actually necessary?
+    cli_abort(c("`earlier_snapshot` and `later_tbl` should have identical column
+                 names and ordering.",
+      "*" = "`earlier_snapshot` colnames: {format_chr_deparse(tbl_names)}",
+      "*" = "`later_tbl` colnames: {format_chr_deparse(names(later_tbl))}"
+    ))
+  }
+
+  combined_tbl <- vec_rbind(earlier_snapshot, later_tbl)
+  combined_n <- nrow(combined_tbl)
+
+  # We'll also need epikeytimes and value columns separately:
+  combined_ukeys <- combined_tbl[ukey_names]
+  combined_vals <- combined_tbl[val_names]
+
+  # We have five types of rows in combined_tbl:
+  # 1. From earlier_snapshot, no matching ukey in later_tbl (deletion; turn vals to
+  #    NAs to match epi_archive format)
+  # 2. From earlier_snapshot, with matching ukey in later_tbl (context; exclude from
+  #    result)
+  # 3. From later_tbl, with matching ukey in earlier_snapshot, with value "close" (change
+  #    that we'll compactify away)
+  # 4. From later_tbl, with matching ukey in earlier_snapshot, value not "close" (change
+  #    that we'll record)
+  # 5. From later_tbl, with no matching ukey in later_tbl (addition)
+
+  # For "snapshot" later_format, we need to filter to 1., 4., and 5., and alter
+  # values for 1.  For "update" later_format, we need to filter to 4. and 5.
+
+  # (For compactify_abs_tol = 0, we could potentially streamline things by dropping
+  # ukey+val duplicates (cases 2. and 3.).)
+
+  # Row indices of first occurrence of each ukey; will be the same as
+  # seq_len(combined_n) except for when that ukey has been re-reported in
+  # `later_tbl`, in which case (3. or 4.) it will point back to the row index of
+  # the same ukey in `earlier_snapshot`:
+  combined_ukey_firsts <- vec_duplicate_id(combined_ukeys)
+
+  # Which rows from combined are cases 3. or 4.?
+  combined_ukey_is_repeat <- combined_ukey_firsts != seq_len(combined_n)
+  # For each row in 3. or 4., row numbers of the ukey appearance in earlier:
+  ukey_repeat_first_i <- combined_ukey_firsts[combined_ukey_is_repeat]
+
+  # Which rows from combined are in case 3.?
+  combined_compactify_away <- rep(FALSE, combined_n)
+  combined_compactify_away[combined_ukey_is_repeat] <-
+    approx_equal0(combined_vals,
+      combined_vals,
+      # TODO move inds closer to vals to not be as confusing?
+      abs_tol = compactify_abs_tol,
+      na_equal = TRUE,
+      inds1 = combined_ukey_is_repeat,
+      inds2 = ukey_repeat_first_i
+    )
+
+  # Which rows from combined are in cases 3., 4., or 5.?
+  combined_from_later <- vec_rep_each(c(FALSE, TRUE), c(earlier_n, later_n))
+
+  if (later_format == "update") {
+    # Cases 4. and 5.:
+    combined_tbl <- combined_tbl[combined_from_later & !combined_compactify_away, ]
+  } else { # later_format == "snapshot"
+    # Which rows from combined are in case 1.?
+    combined_is_deletion <- vec_rep_each(c(TRUE, FALSE), c(earlier_n, later_n))
+    combined_is_deletion[ukey_repeat_first_i] <- FALSE
+    # Which rows from combined are in cases 1., 4., or 5.?
+    combined_include <- combined_is_deletion | combined_from_later & !combined_compactify_away
+    combined_tbl <- combined_tbl[combined_include, ]
+    # Represent deletion in 1. with NA-ing of all value columns. (In some
+    # previous approaches to epi_diff2, this seemed to be faster than using
+    # vec_rbind(case_1_ukeys, cases_45_tbl) or bind_rows to fill with NAs, and more
+    # general than data.table's rbind(case_1_ukeys, cases_45_tbl, fill = TRUE).)
+    combined_tbl[combined_is_deletion[combined_include], val_names] <- NA
+  }
+
+  combined_tbl
+}
+
+epi_diff2 <- function(earlier_snapshot, later_edf,
+                      later_format = c("snapshot", "update"),
+                      compactify_abs_tol = 0) {
+  ukey_names <- key_colnames(later_edf)
+  dplyr_reconstruct(tbl_diff2(as_tibble(earlier_snapshot), as_tibble(later_edf), ukey_names, later_format, compactify_abs_tol), later_edf)
+}
+
+# XXX vs. tbl_patch_apply?
+
+
+
+#' Apply an update (e.g., from `tbl_diff2`) to a snapshot
+#'
+#' @param snapshot tibble or `NULL`; entire data set as of some version, or
+#'   `NULL` to treat `update` as the initial version of the data set.
+#' @param update tibble; ukeys + initial values for added rows, ukeys + new
+#'   values for changed rows. Deletions must be imprecisely represented as
+#'   changing all values to NAs.
+#' @param ukey_names character; names of columns that should form a unique key
+#'   for `snapshot` and for `update`. Uniqueness is unchecked; if you don't have
+#'   this guaranteed, see [`check_ukey_unique()`].
+#' @return tibble; snapshot of the data set with the update applied.
+tbl_patch <- function(snapshot, update, ukey_names) {
+  # Most input validation. This is a small function so use faster validation
+  # variants:
+  if (!is_tibble(update)) {
+    # XXX debating about whether to have a specialized class for updates/diffs.
+    # Seems nice for type-based reasoning and might remove some args from
+    # interfaces, but would require constructor/converter functions for that
+    # type.
+    cli_abort("`update` must be a tibble")
+  }
+  if (is.null(snapshot)) {
+    return(update)
+  }
+  if (!is_tibble(snapshot)) {
+    cli_abort("`snapshot` must be a tibble")
+  }
+
+  result_tbl <- vec_rbind(update, snapshot)
+
+  dup_ids <- vec_duplicate_id(result_tbl[ukey_names])
+  not_overwritten <- dup_ids == vec_seq_along(result_tbl)
+  result_tbl <- result_tbl[not_overwritten, ]
+
+  ## result_tbl <- arrange_canonical(result_tbl)
+
+  result_tbl
+}
+
+epi_patch <- function(snapshot, update) {
+  ukey_names <- key_colnames(update)
+  dplyr_reconstruct(tbl_patch(as_tibble(snapshot), as_tibble(update), ukey_names), update)
+}
