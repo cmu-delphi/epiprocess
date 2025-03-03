e59e2e97da4812092a6d51df1af46b23a5f6f330
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2025-03-03 10:09:01 -0800

    feat: support before = Inf in epix_epi_slide_opt

--- R/epix_epi_slide_opt.R
+++ R/epix_epi_slide_opt.R
@@ -1,7 +1,5 @@
+# TODO just make this an epi_slide_opt impl?
 epix_epi_slide_opt_one_epikey <- function(updates, in_colnames, f, f_from_package, before, after, time_type, out_colnames) {
-  if (before == Inf) {
-    cli_abort("epiprocess internal error: epix_epi_slide_opt_one_epikey() called with before == Inf")
-  }
   unit_step <- epiprocess:::unit_time_delta(time_type)
   prev_inp_snapshot <- NULL
   prev_out_snapshot <- NULL
@@ -11,25 +9,41 @@ epix_epi_slide_opt_one_epikey <- function(updates, in_colnames, f, f_from_packag
     ## setDF(inp_update)
     ## inp_update <- new_tibble(inp_update, nrow = nrow(inp_update))
     inp_snapshot <- tbl_patch(prev_inp_snapshot, inp_update, "time_value")
-    inp_update_min_t <- min(inp_update$time_value) # TODO check efficiency
-    inp_update_max_t <- max(inp_update$time_value)
-    # If the input had updates in the range t1..t2, this could produce changes
-    # in slide outputs in the range t1-after..t2+before, and to compute those
-    # slide values, we need to look at the input snapshot from
-    # t1-after-before..t2+before+after.
-    slide_min_t <- inp_update_min_t - (before + after) * unit_step
-    slide_max_t <- inp_update_max_t + (before + after) * unit_step
-    slide_n <- time_delta_to_n_steps(slide_max_t - slide_min_t, time_type) + 1L
-    slide_time_values <- slide_min_t + 0L:(slide_n - 1L) * unit_step
+    if (before == Inf) {
+      if (after != 0) {
+        cli_abort('.window_size = Inf is only supported with .align = "right"')
+      }
+      # We need to use the entire input snapshot range, filling in time gaps. We
+      # shouldn't pad the ends.
+      slide_min_t <- min(inp_snapshot$time_value) # TODO check efficiency
+      slide_max_t <- max(inp_snapshot$time_value)
+    } else {
+      # If the input had updates in the range t1..t2, this could produce changes
+      # in slide outputs in the range t1-after..t2+before, and to compute those
+      # slide values, we need to look at the input snapshot from
+      # t1-after-before..t2+before+after.
+      inp_update_min_t <- min(inp_update$time_value) # TODO check efficiency
+      inp_update_max_t <- max(inp_update$time_value)
+      slide_min_t <- inp_update_min_t - (before + after) * unit_step
+      slide_max_t <- inp_update_max_t + (before + after) * unit_step
+    }
+    slide_nrow <- time_delta_to_n_steps(slide_max_t - slide_min_t, time_type) + 1L
+    slide_time_values <- slide_min_t + 0L:(slide_nrow - 1L) * unit_step
     slide_inp_backrefs <- vec_match(slide_time_values, inp_snapshot$time_value)
     slide <- inp_snapshot[slide_inp_backrefs, ] # TODO vs. DT key index vs ....
     slide$time_value <- slide_time_values
     # TODO ensure before & after as integers?
     # TODO parameterize naming, slide function, options, ...
     if (f_from_package == "data.table") {
+      if (before == Inf) {
+        n_arg <- seq_len(slide_nrow)
+        adaptive_arg <- TRUE
+      } else {
+        n_arg <- before + after + 1L
+        adaptive_arg <- FALSE
+      }
       for (col_i in seq_along(in_colnames)) {
-        # FIXME other arg forwarding
-        slide[[out_colnames[[col_i]]]] <- f(slide[[in_colnames[[col_i]]]], before + after + 1L)
+        slide[[out_colnames[[col_i]]]] <- f(slide[[in_colnames[[col_i]]]], n_arg, adaptive = adaptive_arg)
       }
     } else if (f_from_package == "slider") {
       for (col_i in seq_along(in_colnames)) {
@@ -41,21 +55,26 @@ epix_epi_slide_opt_one_epikey <- function(updates, in_colnames, f, f_from_packag
         #
         # FIXME can this generate an error on very short series?
         vec_slice(out_col, seq_len(before)) <- NA
-        vec_slice(out_col, slide_n - after + seq_len(after)) <- NA
+        vec_slice(out_col, slide_nrow - after + seq_len(after)) <- NA
         slide[[out_colnames[[col_i]]]] <- out_col
       }
     } else {
       cli_abort("epiprocess internal error: `f_from_package` was {format_chr_deparse(f_from_package)}")
     }
-    out_update <- slide[
-      # Get back to t1-after..t2+before; times outside this range were included
-      # only so those inside would have enough context for their slide
-      # computations, but these "context" rows may contain invalid slide
-      # computation outputs:
-      vec_rep_each(c(FALSE, TRUE, FALSE), c(before, slide_n - before - after, after)) &
-        # Only include time_values that appeared in the input snapshot:
-        !is.na(slide_inp_backrefs),
-    ]
+    rows_should_keep <-
+      if (before == Inf) {
+        # Re-introduce time gaps:
+        !is.na(slide_inp_backrefs)
+      } else {
+        # Get back to t1-after..t2+before; times outside this range were included
+        # only so those inside would have enough context for their slide
+        # computations, but these "context" rows may contain invalid slide
+        # computation outputs:
+        vec_rep_each(c(FALSE, TRUE, FALSE), c(before, slide_nrow - before - after, after)) &
+          # Only include time_values that appeared in the input snapshot:
+          !is.na(slide_inp_backrefs)
+      }
+    out_update <- slide[rows_should_keep, ]
     out_diff <- tbl_diff2(prev_out_snapshot, out_update, "time_value", "update") # TODO avoid redundant diff2 work? though depends on compactify parms...
     out_snapshot <- tbl_patch(prev_out_snapshot, out_diff)
     prev_inp_snapshot <<- inp_snapshot
