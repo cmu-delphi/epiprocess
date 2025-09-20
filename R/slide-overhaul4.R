e68e97fd3acbc8a6762704f577163a17ba3c0540
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2025-09-19 17:58:51 -0700

    Consider some possible comp result sizing refactors

--- R/slide-overhaul4.R
+++ R/slide-overhaul4.R
@@ -1,16 +1,70 @@
 
+apply_result_common_sizer <- function(quosure_result_raw, sizer, results_env) {
+  # We want something like `dplyr_col_modify()` but allowing recycling
+  # of previous computations and updating `results_env` and unpacking
+  # tibbles if not manually named.
+  if (!is.null(common_size)) {
+    # XXX could improve error messages here
+    quosure_result_recycled <- vctrs::vec_recycle(quosure_result_raw, common_size)
+  } else {
+    quosure_result_recycled <- quosure_result_raw
+    quosure_result_size <- vctrs::vec_size(quosure_result_raw)
+    if (quosure_result_size != 1L) {
+      common_size <- quosure_result_size
+      for (previous_result_nm in names(results_env)) {
+        results_env[[previous_result_nm]] <- vctrs::vec_recycle(results_env[[previous_result_nm]], common_size)
+      }
+    } # else `common_size` remains NULL
+  }
+  return(list(quosure_result_recycled, common_size))
+  # return(quosure_result_recycled) # TODO + mutable sizer object?
+}
 
-apply_comp_quosures <- function(data_mask, comp_quos, size_spec) {
+# new_common_size_result_builder <- function() {
+#   results_env <- new.env(parent = emptyenv())
+#   common_size <- NULL
+#   function(quosure_result_raw) {
+#   }
+#   but what about binding?
+# }
+
+new_result_common_sizer <- function(results_env) {
   common_size <- NULL
+  function(quosure_result_raw) {
+    # We want something like `dplyr_col_modify()` but allowing recycling
+    # of previous computations and updating `results_env` and unpacking
+    # tibbles if not manually named.
+    if (!is.null(common_size)) {
+      # XXX could improve error messages here
+      quosure_result_recycled <- vctrs::vec_recycle(quosure_result_raw, common_size)
+    } else {
+      quosure_result_recycled <- quosure_result_raw
+      quosure_result_size <- vctrs::vec_size(quosure_result_raw)
+      if (quosure_result_size != 1L) {
+        common_size <- quosure_result_size
+        for (previous_result_nm in names(results_env)) {
+          results_env[[previous_result_nm]] <- vctrs::vec_recycle(results_env[[previous_result_nm]], common_size)
+        }
+      } # else `common_size` remains NULL
+    }
+    return(quosure_result_recycled)
+  }
+}
+
+apply_comp_quosures <- function(data_mask, comp_quos, common_size) {
+  # common_size <- NULL
   # The data mask is an environment; it doesn't track the binding order.
   # We'll track that separately. For efficiency, we'll use `c` to add to
   # this order, and deal with binding redefinitions at the end. We'll
   # reflect deletions immediately (current implementation of `new_tibble`
   # seems like it would exclude `NULL` bindings for us but `?new_tibble`
   # doesn't reflect this behavior).
+  results_env <- new.env(parent = emptyenv())
+  sizer <- new_result_common_sizer(results_env)
   results_multiorder <- character(0L)
-  for (quosure_i in seq_along(dots_quos)) {
-    quosure_result_raw <- rlang::eval_tidy(dots_quos[[quosure_i]], data_mask)
+  nms <- names(comp_quos)
+  for (quosure_i in seq_along(comp_quos)) {
+    quosure_result_raw <- rlang::eval_tidy(comp_quos[[quosure_i]], data_mask)
     if (is.null(quosure_result_raw)) {
       nm <- nms[[quosure_i]]
       results_multiorder <- vctrs::vec_set_difference(results_multiorder, nm)
@@ -22,24 +76,29 @@ apply_comp_quosures <- function(data_mask, comp_quos, size_spec) {
       is.data.frame(quosure_result_raw) ||
         vctrs::obj_is_vector(quosure_result_raw) && is.null(vctrs::vec_names(quosure_result_raw))
     ) {
-      # We want something like `dplyr_col_modify()` but allowing recycling
-      # of previous computations and updating `results_env` and unpacking
-      # tibbles if not manually named.
-      if (!is.null(common_size)) {
-        # XXX could improve error messages here
-        quosure_result_recycled <- vctrs::vec_recycle(quosure_result_raw, common_size)
-      } else {
-        quosure_result_recycled <- quosure_result_raw
-        quosure_result_size <- vctrs::vec_size(quosure_result_raw)
-        if (quosure_result_size != 1L) {
-          common_size <- quosure_result_size
-          for (previous_result_nm in names(results_env)) {
-            results_env[[previous_result_nm]] <- vctrs::vec_recycle(results_env[[previous_result_nm]], common_size)
-          }
-        } # else `common_size` remains NULL
-      }
-      stop("TODO size adjustment seems like what needs to be replaceable with multisize")
-      stop("TODO what about data mask stuff?  multicomp needing multimask or submask reformulation? or is there way to avoid?")
+      # # We want something like `dplyr_col_modify()` but allowing recycling
+      # # of previous computations and updating `results_env` and unpacking
+      # # tibbles if not manually named.
+      # if (!is.null(common_size)) {
+      #   # XXX could improve error messages here
+      #   quosure_result_recycled <- vctrs::vec_recycle(quosure_result_raw, common_size)
+      # } else {
+      #   quosure_result_recycled <- quosure_result_raw
+      #   quosure_result_size <- vctrs::vec_size(quosure_result_raw)
+      #   if (quosure_result_size != 1L) {
+      #     common_size <- quosure_result_size
+      #     for (previous_result_nm in names(results_env)) {
+      #       results_env[[previous_result_nm]] <- vctrs::vec_recycle(results_env[[previous_result_nm]], common_size)
+      #     }
+      #   } # else `common_size` remains NULL
+      # }
+      # temp <- apply_single_size_spec(quosure_result_raw, common_size, results_env)
+      # quosure_result_recycled <- temp[[1L]]
+      # common_size <- temp[[2L]]
+      # quosure_result_recycled <- apply_single_size_spec(quosure_result_raw, common_size, results_env)
+      quosure_result_recycled <- sizer(quosure_result_raw)
+      # stop("TODO size adjustment seems like what needs to be replaceable with multisize")
+      # stop("TODO what about data mask stuff?  multicomp needing multimask or submask reformulation? or is there way to avoid?")
       # Idea is that fast slides would be multicomps with a regular
       # data_mask (for the group) and multisize handling, while
       # (sequences of) slow window comp args would be multicomps
@@ -61,7 +120,7 @@ apply_comp_quosures <- function(data_mask, comp_quos, size_spec) {
     } else {
       cli_abort("
             Problem with output of {.code
-            {rlang::expr_deparse(rlang::quo_get_expr(dots_quos[[quosure_i]]))}}; it
+            {rlang::expr_deparse(rlang::quo_get_expr(comp_quos[[quosure_i]]))}}; it
             produced a result that was neither NULL, a data.frame, nor a vector
             without unnamed entries (as determined by the vctrs package).
           ", class = "epiprocess__invalid_slide_comp_tidyeval_output")
