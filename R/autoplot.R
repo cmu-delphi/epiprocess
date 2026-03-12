56372eb58bd2c4836b66b83f6be1e51381034066
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2026-03-12 15:43:15 -0700

    Comment and message in `autoplot.epi_archive` `.versions` handling

--- R/autoplot.R
+++ R/autoplot.R
@@ -496,11 +496,16 @@ autoplot.epi_archive <- function(object, ...,
 
   tt_lookup <- c("day" = "day", "week" = "week", "yearmonth" = "month")
   .versions <- .versions %||% ifelse(time_type == "integer", 1L, unname(tt_lookup[time_type]))
-  if (is.character(.versions) || length(.versions) == 1L) {
+  if ((is.character(.versions) || rlang::is_bare_numeric(.versions)) && length(.versions) == 1L) {
+    # Interpret `.versions` as a period (even if archive versions are also bare numeric...)
     if (is.numeric(.versions)) .versions <- round(abs(.versions))
     .versions <- seq(min_version, max_version - 1, by = .versions)
-  } else if (inherits(.versions, "Date") || is.numeric(.versions)) {
+  } else if (inherits(.versions, "Date") || inherits(.versions, "Date")) {
+    old_n_versions <- length(.versions)
     .versions <- .versions[min_version <= .versions & .versions <= max_version]
+    if (length(.versions) != old_n_versions) {
+        cli_inform("Removed entries from `.versions` that weren't in the range of archive versions with update rows.")
+    }
   } else {
     cli_abort(
       "Requested `.versions` don't appear to match the available `time_type`.",
